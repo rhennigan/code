50b4f16a3f9620e12f9c46db2c7af09775a0acb5
rhennigan <rhennigan@gmail.com>
2014-10-27 13:18:58 -0400

    prototypes of matrix functions

---
+++ math/matrices.h
@@ -0,0 +1,31 @@
+// Copyright 2014, Richard Hennigan
+
+#ifndef MATH_MATRICES_H_
+#define MATH_MATRICES_H_
+
+#include "./vectors.h"
+
+typedef struct matrix64_s {
+  int rows, cols;
+  vector64_t * r;
+} matrix64_t;
+
+typedef struct matrix32_s {
+  int rows, cols;
+  vector32_t * r;
+} matrix32_t;
+
+typedef matrix64_t matrix_t;
+
+matrix_t mat_add(matrix_t m1, matrix_t m2);
+void     mat_add_i(matrix_t * m1, matrix_t m2);
+void     mat_dispose(matrix_t * m);
+vector_t mat_dotvr(matrix_t m, vector_t v);
+matrix_t mat_init(int rows, int cols);
+vector_t mat_mean(matrix_t m);
+vector_t mat_principal_axis(matrix_t m);
+void     mat_print(matrix_t m);
+matrix_t mat_shift(matrix_t m);
+vector_t mat_sum(matrix_t m);
+
+#endif  // MATH_MATRICES_H_
