8568c3b7f51702fe131ade1bfdb20f16d69e19f4
rhennigan <rhennigan@gmail.com>
2014-11-01 09:54:16 -0400

    finished first part

--- classes/cs102/a7/test.c
+++ classes/cs102/a7/test.c
@@ -2,12 +2,14 @@
 // Copyright (C) 2014 Richard Hennigan
 
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lib/list.h"
 #include "lib/pq.h"
+#include "lib/user_input.h"
 
-#define STR_SZ 64
+#define STR_SZ 28
 
 typedef struct date_s {
   int month;
@@ -26,26 +28,42 @@ void print_assignment(void * addr);
 assign_t * new_assign(char * cname, char * aname, int points, date_t due);
 assign_t * new_assign_prompt();
 
-assign_t * new_assign_prompt();
+assign_t * new_assign_prompt() {
+  printf("enter the course name: ");
+  char * cname = get_input_string();
+
+  printf("enter the assignment name: ");
+  char * aname = get_input_string();
+
+  printf("enter the number of points: ");
+  int pts = get_input_int(0, INT_MAX);
+
+  printf("enter the month of the due date (1-12): ");
+  int m = get_input_int(1, 12);
+
+  printf("enter the day of the due date (1-31): ");
+  int d = get_input_int(1, 31);
+
+  return new_assign(cname, aname, pts, (date_t){ m, d });
+}
 
 
 int main() {
   pq_t * pq = pq_init(sizeof(assign_t), &compare);
 
-  assign_t * a1 = new_assign("course 1", "paper", 25, (date_t){11, 5});
-  assign_t * a2 = new_assign("course 2", "project", 15, (date_t){11, 5});
-
-  pq_enqueue(pq, a1);
-  pq_enqueue(pq, a2);
-
-  list_iter(pq->list, &print_assignment);
-  printf("\n\n");
+  int i;
+  for (i = 0; i < 5; i++) {
+    assign_t * a = new_assign_prompt();
+    pq_enqueue(pq, a);
+    printf("\nCourse                      Assignment                  Points\tDue\n");
+    printf("---------------------------------------------------------------------\n");
+    list_iter(pq->list, &print_assignment);
+    printf("\n\n");
+  }
 
   pq_dump(pq);
-
   pq_dispose(pq);
-
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 bool compare(void * addr1, void * addr2) {
@@ -64,13 +82,33 @@ bool compare(void * addr1, void * addr2) {
 
 void print_assignment(void * addr) {
   assign_t * a = addr;
-  printf("assignment:\n");
-  printf("----------------------------\n");
-  printf("  course:     %s\n", a->course_name);
-  printf("  assignment: %s\n", a->assign_name);
-  printf("  points      %d\n", a->points);
-  printf("  due         %d / %d\n", a->due.month, a->due.day);
-  printf("\n");
+  char cname[STR_SZ];
+  char aname[STR_SZ];
+  int i;
+  for (i = 0; i < STR_SZ; i++) {
+    if (a->course_name[i] == '\0') {
+      for (; i < STR_SZ - 1; i++) {
+        cname[i] = ' ';
+      }
+      break;
+    }
+    cname[i] = a->course_name[i];
+  }
+  cname[STR_SZ - 1] = '\0';
+
+  for (i = 0; i < STR_SZ; i++) {
+    if (a->assign_name[i] == '\0') {
+      for (; i < STR_SZ - 1; i++) {
+        aname[i] = ' ';
+      }
+      break;
+    }
+    aname[i] = a->assign_name[i];
+  }
+  aname[STR_SZ - 1] = '\0';
+
+  printf("%s %s %d\t%d/%d\n", cname, aname,
+         a->points, a->due.month, a->due.day);
 }
 
 assign_t * new_assign(char * cname, char * aname, int points, date_t due) {
