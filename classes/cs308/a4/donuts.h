9186e20f1029915a4bf19c27a4fbe8d341a45e7f
rhennigan <rhennigan@gmail.com>
2014-11-04 17:02:42 -0500

    auto-commit changes: Tue Nov  4 17:02:42 EST 2014

---
+++ classes/cs308/a4/donuts.h
@@ -0,0 +1,31 @@
+#ifndef DONUTS_H_
+#define DONUTS_H_
+
+#include <signal.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#define MAXFLAVORS   4
+#define MAXSLOTS     50
+#define MAXCONSUMERS 5
+#define MAXPRODUCERS 5
+
+struct donut_ring {
+  int flavor[MAXFLAVORS][MAXSLOTS];
+  int outptr[MAXFLAVORS];
+  int in_ptr[MAXFLAVORS];
+  int serial[MAXFLAVORS];
+  int spaces[MAXFLAVORS];
+  int donuts[MAXFLAVORS];
+};
+
+/**********************************************************************/
+/* SIGNAL WAITER, PRODUCER AND CONSUMER THREAD FUNCTIONS              */
+/**********************************************************************/
+
+void * sig_waiter(void * arg);
+void * producer(void * arg);
+void * consumer(void * arg);
+void * sig_handler(int sig_num);
+
+#endif  // DONUTS_H_
