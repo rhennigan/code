627715e39122c26acd55c7b8a31c3a5318340980
rhennigan <rhennigan@gmail.com>
2014-11-04 15:52:03 -0500

    auto-commit changes: Tue Nov  4 15:52:03 EST 2014

--- classes/cs308/a4/test.c
+++ classes/cs308/a4/test.c
@@ -6,15 +6,21 @@
 
 #define NTHREADS 8
 
+volatile int running_threads = 0;
+pthread_mutex_t running_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void * print_hello_world(void * tid) {
   int * id = tid;
   double val = 0.0;
   long int i;
-  for (i = 1; i < 10000000000; i++) {
+  for (i = 1; i < 100000000000; i++) {
     val += 1.0 / (double)i;
   }
 
   printf("Thread #%d reults: val = %f\n", *id, val);
+  pthread_mutex_lock(&running_mutex);
+  running_threads--;
+  pthread_mutex_unlock(&running_mutex);
   pthread_exit(NULL);
 }
 
@@ -24,6 +30,9 @@ int main(int argc, char *argv[]) {
   int status, i;
 
   for (i = 0; i < NTHREADS; i++) {
+    pthread_mutex_lock(&running_mutex);
+    running_threads++;
+    pthread_mutex_unlock(&running_mutex);
     printf("Initializing thread #%d\n", i);
     thr_id[i] = i;
     status = pthread_create(&threads[i], NULL, print_hello_world, thr_id + i);
@@ -34,7 +43,10 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  sleep(3);
+  while (running_threads > 0)
+  {
+     sleep(1);
+  }
   exit(EXIT_SUCCESS);
 }
 
