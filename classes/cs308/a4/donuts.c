7aa5ee83f68fdf08d7840e6a661c6402d9a9b1e3
rhennigan <rhennigan@gmail.com>
2014-11-04 17:38:23 -0500

    auto-commit changes: Tue Nov  4 17:38:23 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "./donuts.h"
 
 donut_ring_t shared_ring;
@@ -63,12 +65,12 @@ int main(int argc, char *argv[]) {
   sigprocmask(SIG_BLOCK, &all_signals, NULL);
   sigfillset(&all_signals);
   for (i = 0; i < nsigs; i++) {
-    new_act.sa_handler  = sig_handler;
-    new_act.sa_mask     = all_signals;
-    new_act.sa_flags    = 0;
+    new_act.sa_handler = sig_handler;
+    new_act.sa_mask = all_signals;
+    new_act.sa_flags = 0;
     if (sigaction(sigs[i], &new_act, NULL) == -1) {
       perror("can't set signals: ");
-      exit(1);
+      exit(EXIT_FAILURE);
     }
   }
 
