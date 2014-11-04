7579a7a242104a8ee9e0db9acff4ba599072a3f7
rhennigan <rhennigan@gmail.com>
2014-11-04 18:30:38 -0500

    auto-commit changes: Tue Nov  4 18:30:38 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -157,7 +157,22 @@ void * consumer(void * arg);
 /******************************************************************************/
 /* PTHREAD ASYNCH SIGNAL HANDLER ROUTINE...                                   */
 /******************************************************************************/
-void * sig_waiter(void * arg);
+void * sig_waiter(void * arg) {
+  sigset_t sigterm_signal;
+  int signo;
+
+  sigemptyset(&sigterm_signal);
+  sigaddset(&sigterm_signal, SIGTERM);
+  sigaddset(&sigterm_signal, SIGINT);
+
+  if (sigwait(&sigterm_signal, &signo) != 0) {
+    printf("\n  sigwait() failed, exiting\n");
+    exit(2);
+  }
+  printf("process going down on SIGNAL (number %d)\n\n", signo);
+  exit(1);
+  return NULL;
+}
 
 /******************************************************************************/
 /* PTHREAD SYNCH SIGNAL HANDLER ROUTINE...                                    */
