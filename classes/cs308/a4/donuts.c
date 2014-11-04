87b4e8925f22f86c1c2694cc255fb9a7b216b681
rhennigan <rhennigan@gmail.com>
2014-11-04 18:04:56 -0500

    auto-commit changes: Tue Nov  4 18:04:56 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -141,5 +141,23 @@ int main(int argc, char *argv[]) {
   for (i = 1; i < numconsumers + 1; i++)
     pthread_join(thread_id[i], NULL);
 
-  return 0;
+  /*****************************************************************/
+  /* GET FINAL TIMESTAMP, CALCULATE ELAPSED SEC AND USEC           */
+  /*****************************************************************/
+
+  gettimeofday(&last_time, (struct timezone *)0);
+  if ((i = last_time.tv_sec - first_time.tv_sec) == 0) {
+  j = last_time.tv_usec - first_time.tv_usec;
+} else {
+  if ( last_time.tv_usec - first_time.tv_usec < 0 ) {
+  i--;
+  j = 1000000 + 
+      ( last_time.tv_usec - first_time.tv_usec );
+} else {
+  j = last_time.tv_usec - first_time.tv_usec; }
+}
+  printf ( "Elapsed consumer time is %d sec and %d usec\n", i, j );
+
+  printf ( "\n\n ALL CONSUMERS FINISHED, KILLING  PROCESS\n\n" );
+  exit ( 0 );
 }
