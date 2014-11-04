ea0ca4349674111f88813542d02de1fbbed57ef6
rhennigan <rhennigan@gmail.com>
2014-11-04 17:29:09 -0500

    auto-commit changes: Tue Nov  4 17:29:09 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -35,5 +35,21 @@ int main(int argc, char *argv[]) {
     arg_array[i] = i;  /* SET ARRAY OF ARGUMENT VALUES */
   }
 
+  /**********************************************************************/
+  /* GENERAL PTHREAD MUTEX AND CONDITION INIT AND GLOBAL INIT           */
+  /**********************************************************************/
+
+  for (i = 0; i < numflavors; i++) {
+    pthread_mutex_init ( &prod [i], NULL );
+    pthread_mutex_init ( &cons [i], NULL );
+    thread_cond_init ( &prod_cond [i],  NULL );
+    pthread_cond_init ( &cons_cond [i],  NULL );
+    shared_ring.outptr [i]		= 0;
+    shared_ring.in_ptr [i]		= 0;
+    shared_ring.serial [i]		= 0;
+    shared_ring.spaces [i]		= NUMSLOTS;
+    shared_ring.donuts [i]		= 0;
+  }
+
   return 0;
 }
