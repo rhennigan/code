7de4925e0538b805591edf574cb72e95119fe203
rhennigan <rhennigan@gmail.com>
2014-11-04 17:31:12 -0500

    auto-commit changes: Tue Nov  4 17:31:12 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[]) {
   struct sched_param shed_struct;
 
   int i, j, k;
+
   /**********************************************************************/
   /* INITIAL TIMESTAMP VALUE FOR PERFORMANCE MEASURE                    */
   /**********************************************************************/
@@ -42,13 +43,13 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < numflavors; i++) {
     pthread_mutex_init(&prod[i], NULL);
     pthread_mutex_init(&cons[i], NULL);
-    thread_cond_init ( &prod_cond [i],  NULL );
-    pthread_cond_init ( &cons_cond [i],  NULL );
-    shared_ring.outptr [i]		= 0;
-    shared_ring.in_ptr [i]		= 0;
-    shared_ring.serial [i]		= 0;
-    shared_ring.spaces [i]		= NUMSLOTS;
-    shared_ring.donuts [i]		= 0;
+    pthread_cond_init(&prod_cond[i], NULL);
+    pthread_cond_init(&cons_cond[i], NULL);
+    shared_ring.outptr[i] = 0;
+    shared_ring.in_ptr[i] = 0;
+    shared_ring.serial[i] = 0;
+    shared_ring.spaces[i] = numslots;
+    shared_ring.donuts[i] = 0;
   }
 
   return 0;
