0a9f4b8f91469d438585d3be530cab5c5cf4806c
rhennigan <rhennigan@gmail.com>
2014-11-05 04:32:15 -0500

    auto-commit changes: Wed Nov  5 04:32:15 EST 2014

--- classes/cs308/a4/loop.sh
+++ classes/cs308/a4/loop.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-for i in `seq 100 5000 100`;
+for i in `seq 100 100 5000`;
 do
 	count=0
 	for j in `seq 1 100`;
 	do
 		res=$(`echo ./donuts $i 50 30 200`)
-		echo $res
 		count=`expr $count + $res`
 	done
 	echo $i", "$count
