1d67b75a94f6e6052124a5e0388fb0268d132a62
rhennigan <rhennigan@gmail.com>
2014-11-04 23:22:08 -0500

    auto-commit changes: Tue Nov  4 23:22:08 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -115,6 +115,7 @@ int main(/* int argc, char *argv[] */) {
 
   /* create all the producer threads */
   for (i = 0; i < numproducers; i++) {
+    printf("creating producer %d\n", i);
     if (pthread_create(&thread_id[i], &th_attr, producer, NULL) != 0) {
       printf("pthread_create failed ");
       exit(3);
@@ -123,6 +124,7 @@ int main(/* int argc, char *argv[] */) {
 
   /* create all the consumer threads */
   for (i = numproducers; i < numconsumers + numproducers; i++) {
+    printf("creating consumer %d\n", i);
     if (pthread_create(&thread_id[i], &th_attr, consumer,
       (void *)&arg_array[i]) != 0) {
       printf("pthread_create failed");
