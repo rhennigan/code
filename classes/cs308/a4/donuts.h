22b3f67141c35b83f2744dad5c5d0a982da652a4
rhennigan <rhennigan@gmail.com>
2014-11-04 20:27:07 -0500

    auto-commit changes: Tue Nov  4 20:27:07 EST 2014

--- classes/cs308/a4/donuts.h
+++ classes/cs308/a4/donuts.h
@@ -29,6 +29,7 @@ void * sig_waiter(void * arg);
 void * producer(void * arg);
 void * consumer(void * arg);
 void   sig_handler(int sig_num);
+void   output_collection(int ** collection);
 
 /**********************************************************************/
 /* PTHREAD ROUTINE ARGUMENT STRUCTS                                   */
