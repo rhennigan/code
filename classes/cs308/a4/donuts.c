ebaaf5cfef5a7a6379ed31d30fec11b0353158bb
rhennigan <rhennigan@gmail.com>
2014-11-05 01:39:22 -0500

    auto-commit changes: Wed Nov  5 01:39:22 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -294,7 +294,7 @@ void * consumer(void * arg) {
       pthread_mutex_lock(&cons[sel]);
 
       /* if there are no donuts available, thread will wait until signaled */
-      while (shared_ring.donuts[sel] == 0) {
+      while (!need_quit && shared_ring.donuts[sel] == 0) {
         pthread_cond_wait(&cons_cond[sel], &cons[sel]);
         /* producer must signal cons_cond[sel] when available */
       }
