59b16c3a901d9b52dacb4b337b44abfa6bac33d7
rhennigan <rhennigan@gmail.com>
2014-11-04 21:40:44 -0500

    auto-commit changes: Tue Nov  4 21:40:44 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -146,18 +146,18 @@ int main(/* int argc, char *argv[] */) {
   /* GET FINAL TIMESTAMP, CALCULATE ELAPSED SEC AND USEC                      */
   /****************************************************************************/
   gettimeofday(&last_time, (struct timezone *)0);
-  if ((i = last_time.tv_sec - first_time.tv_sec) == 0) {
-    j = last_time.tv_usec - first_time.tv_usec;
-  } else {
-    if (last_time.tv_usec - first_time.tv_usec < 0) {
-      i--;
-      j = 1000000 + (last_time.tv_usec - first_time.tv_usec);
-    } else {
-      j = last_time.tv_usec - first_time.tv_usec;
-    }
-  }
+  /* if ((i = last_time.tv_sec - first_time.tv_sec) == 0) { */
+  /*   j = last_time.tv_usec - first_time.tv_usec; */
+  /* } else { */
+  /*   if (last_time.tv_usec - first_time.tv_usec < 0) { */
+  /*     i--; */
+  /*     j = 1000000 + (last_time.tv_usec - first_time.tv_usec); */
+  /*   } else { */
+  /*     j = last_time.tv_usec - first_time.tv_usec; */
+  /*   } */
+  /* } */
 
-  printf("first_time.tv_sec = %d\n", first_time.tv_sec);
+  long int sec, usec;
   printf("Elapsed consumer time is %d sec and %d usec\n", i, j);
 
   printf("\n\n ALL CONSUMERS FINISHED, KILLING  PROCESS\n\n");
