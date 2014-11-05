0fe3178c46821324be57c5c4efe6c730ddabf0ee
rhennigan <rhennigan@gmail.com>
2014-11-04 21:42:47 -0500

    auto-commit changes: Tue Nov  4 21:42:47 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -147,14 +147,14 @@ int main(/* int argc, char *argv[] */) {
   /****************************************************************************/
   gettimeofday(&last_time, (struct timezone *)0);
   long int sec, usec;
-  if ((i = last_time.tv_sec - first_time.tv_sec) == 0) {
-    j = last_time.tv_usec - first_time.tv_usec;
+  if ((sec = last_time.tv_sec - first_time.tv_sec) == 0) {
+    usec = last_time.tv_usec - first_time.tv_usec;
   } else {
     if (last_time.tv_usec - first_time.tv_usec < 0) {
-      i--;
-      j = 1000000 + (last_time.tv_usec - first_time.tv_usec);
+      sec--;
+      usec = 1000000 + (last_time.tv_usec - first_time.tv_usec);
     } else {
-      j = last_time.tv_usec - first_time.tv_usec;
+      usec = last_time.tv_usec - first_time.tv_usec;
     }
   }
 
