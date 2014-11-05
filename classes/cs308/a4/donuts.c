36fa316d3b1a0d0e96f26500268c816ca607c253
rhennigan <rhennigan@gmail.com>
2014-11-04 23:47:46 -0500

    auto-commit changes: Tue Nov  4 23:47:46 EST 2014

--- classes/cs308/a4/donuts.c
+++ classes/cs308/a4/donuts.c
@@ -404,7 +404,7 @@ long int elapsed_us(struct timeval *t2, struct timeval *t1) {
 }
 
 void check_in(int tid) {
-  pthread_mutex_lock(&check_mutx);
-  gettimeofday(&check_time, (struct timezone *)0);
+  pthread_mutex_lock(&check_mutx[tid]);
+  gettimeofday(&check_time[tid], (struct timezone *)0);
   pthread_mutex_unlock(&check_mtx);
 }
