4cc328c86620a9dd25fd8f0a57254e50b096989b
rhennigan <rhennigan@gmail.com>
2014-11-05 04:49:36 -0500

    auto-commit changes: Wed Nov  5 04:49:36 EST 2014

--- classes/cs308/a4/loop.sh
+++ classes/cs308/a4/loop.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-for i in `seq 500 500 10000`;
+for k in `seq 1 100`;
 do
-	count=0
-	for j in `seq 1 100`;
+	for i in `seq 500 500 10000`;
 	do
-		res=$(`echo ./donuts $i 50 30 200`)
-		count=`expr $count + $res`
+		count=0
+		for j in `seq 1 100`;
+		do
+			res=$(`echo ./donuts $i 50 30 200`)
+			count=`expr $count + $res`
+		done
+		echo $i", "$count
+		echo $i", "$count >> "deadlocks.csv"
 	done
-	echo $i", "$count >> "deadlocks.csv"
 done
