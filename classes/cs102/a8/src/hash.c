90664e428cd86047fdfd9995b96f230870f9a29e
rhennigan <rhennigan@gmail.com>
2014-11-07 16:10:42 -0500

    auto-commit changes: Fri Nov  7 16:10:42 EST 2014

--- classes/cs102/a8/src/hash.c
+++ classes/cs102/a8/src/hash.c
@@ -47,6 +47,7 @@ bool key_eq(void * a1, void * a2) {
 
 void * hash_table_lookup(hash_table_t * ht, hkey_t key) {
   uint64_t h_idx = hash(key.key, key.size) % ht->size;
+  printf("\nh_idx = %d\n", h_idx);
   void * found = list_find(ht->row[h_idx], &key, &key_eq);
   return found;
 }
