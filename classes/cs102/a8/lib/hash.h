017adb71191f5775834cd664ec53c917cab53e98
rhennigan <rhennigan@gmail.com>
2014-11-07 13:06:59 -0500

    auto-commit changes: Fri Nov  7 13:06:59 EST 2014

--- classes/cs102/a8/lib/hash.h
+++ classes/cs102/a8/lib/hash.h
@@ -22,6 +22,7 @@ uint64_t       hash(char * str);
 hash_table_t * hash_table_init(uint32_t size);
 void           hash_table_insert(hash_table_t * ht, key_val_t kv);
 void *         hash_table_lookup(hash_table_t * ht, void * key);
+key_val_t *    make_kv(void * key, void * val);
 uint32_t       string_distance(char *s1, char *s2);
 
 #undef HASH_TABLE_SIZE
