299df70726bd5ecdc1f82cfa4a99b5bc47a727c0
rhennigan <rhennigan@gmail.com>
2014-11-07 09:25:44 -0500

    auto-commit changes: Fri Nov  7 09:25:44 EST 2014

--- classes/cs102/a8/src/hash.c
+++ classes/cs102/a8/src/hash.c
@@ -14,19 +14,19 @@ uint64_t hash(char * str) {
 #define MIN3(a, b, c) MIN(MIN(a, b), c)
 
 int levenshtein(char *s1, char *s2) {
-    unsigned int s1len, s2len, x, y, lastdiag, olddiag;
-    s1len = strlen(s1);
-    s2len = strlen(s2);
-    unsigned int column[s1len+1];
-    for (y = 1; y <= s1len; y++)
-        column[y] = y;
-    for (x = 1; x <= s2len; x++) {
-        column[0] = x;
-        for (y = 1, lastdiag = x-1; y <= s1len; y++) {
-            olddiag = column[y];
-            column[y] = MIN3(column[y] + 1, column[y-1] + 1, lastdiag + (s1[y-1] == s2[x-1] ? 0 : 1));
-            lastdiag = olddiag;
-        }
+  uint32_t s1len, s2len, x, y, ld, od;
+  s1len = strlen(s1);
+  s2len = strlen(s2);
+  uint32_t c[s1len+1];
+  for (y = 1; y <= s1len; y++)
+    c[y] = y;
+  for (x = 1; x <= s2len; x++) {
+    c[0] = x;
+    for (y = 1, ld = x-1; y <= s1len; y++) {
+      od = c[y];
+      c[y] = MIN3(c[y] + 1, c[y-1] + 1, ld + (s1[y-1] == s2[x-1] ? 0 : 1));
+      ld = od;
     }
-    return(column[s1len]);
+  }
+  return(c[s1len]);
 }
