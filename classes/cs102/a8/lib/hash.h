fc0428e29699fee9ada3edcd3aaa875c535540fb
rhennigan <rhennigan@gmail.com>
2014-11-07 12:50:30 -0500

    auto-commit changes: Fri Nov  7 12:50:30 EST 2014

--- classes/cs102/a8/lib/hash.h
+++ classes/cs102/a8/lib/hash.h
@@ -15,6 +15,8 @@ typedef struct hash_table_s {
 
 uint64_t       hash(char * str);
 hash_table_t * hash_table_init(uint32_t size);
+void           hash_table_insert(hash_table_t * ht, void * item);
+void *         hash_table_lookup(hash_table_t * ht, void * item);
 uint32_t       string_distance(char *s1, char *s2);
 
 #endif  // LIB_HASH_
