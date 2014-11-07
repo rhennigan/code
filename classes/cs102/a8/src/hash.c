5f0da701a8ad01f880c2c5671e745ffdbbee6719
rhennigan <rhennigan@gmail.com>
2014-11-07 16:39:19 -0500

    auto-commit changes: Fri Nov  7 16:39:19 EST 2014

--- classes/cs102/a8/src/hash.c
+++ classes/cs102/a8/src/hash.c
@@ -72,6 +72,7 @@ key_val_t * make_kv(void * key, size_t ks, void * val, size_t vs) {
   return kv;
 }
 
+/* Levenshtein distance */
 uint32_t string_distance(char *s1, char *s2) {
   uint32_t len1, len2, i, j, ld, od;
   len1 = strlen(s1);
