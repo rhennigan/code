46d8f6b5c086271267cd4b023776f2a015deb76c
rhennigan <rhennigan@gmail.com>
2014-11-07 12:46:25 -0500

    auto-commit changes: Fri Nov  7 12:46:25 EST 2014

--- classes/cs102/a8/lib/hash.h
+++ classes/cs102/a8/lib/hash.h
@@ -4,6 +4,11 @@
 #include <stdint.h>
 #include "./list.h"
 
+typedef struct key_val_s {
+  void * key;
+  void * val;
+} key_val_t;
+
 uint64_t hash(char * str);
 uint32_t string_distance(char *s1, char *s2);
 
