f0ba7e16371dd96e35ee36d7b5a5ab2430b942af
rhennigan <rhennigan@gmail.com>
2014-11-06 20:46:20 -0500

    auto-commit changes: Thu Nov  6 20:46:20 EST 2014

--- classes/cs102/a8/test.c
+++ classes/cs102/a8/test.c
@@ -11,10 +11,20 @@ void print_hash(char * str) {
 int main(int argc, char * argv[]) {
   uint64_t entry_counts[MODSZ];
   int i;
+
+  for (i = 0; i < MODSZ; i++) {
+    entry_counts[i] = 0L;
+  }
+
   for (i = 1; i < argc; i++) {
     print_hash(argv[i]);
     entry_counts[hash(argv[i]) % MODSZ]++;
   }
+
+  for (i = 0; i < MODSZ; i++) {
+    printf("%lu\n", entry_counts[i]);
+  }
+
 
   return 0;
 }
