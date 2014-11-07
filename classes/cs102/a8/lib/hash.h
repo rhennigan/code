9eec9f3235971abb0b728378c66fb9254b8bd2f8
rhennigan <rhennigan@gmail.com>
2014-11-07 13:08:00 -0500

    auto-commit changes: Fri Nov  7 13:08:00 EST 2014

--- classes/cs102/a8/lib/hash.h
+++ classes/cs102/a8/lib/hash.h
@@ -2,6 +2,7 @@
 #define LIB_HASH_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "./list.h"
 
 #define HASH_TABLE_SIZE 100
@@ -23,6 +24,7 @@ hash_table_t * hash_table_init(uint32_t size);
 void           hash_table_insert(hash_table_t * ht, key_val_t kv);
 void *         hash_table_lookup(hash_table_t * ht, void * key);
 key_val_t *    make_kv(void * key, void * val);
+bool           match_key(key_val_t * kv1, key_val_t * kv2);
 uint32_t       string_distance(char *s1, char *s2);
 
 #undef HASH_TABLE_SIZE
