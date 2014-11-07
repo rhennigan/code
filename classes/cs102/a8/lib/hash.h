c8c62daf20e90d37e38813b23fc2d5da38a9c0ae
rhennigan <rhennigan@gmail.com>
2014-11-07 12:54:38 -0500

    auto-commit changes: Fri Nov  7 12:54:38 EST 2014

--- classes/cs102/a8/lib/hash.h
+++ classes/cs102/a8/lib/hash.h
@@ -4,13 +4,15 @@
 #include <stdint.h>
 #include "./list.h"
 
+#define HASH_TABLE_SIZE 100
+
 typedef struct key_val_s {
   void * key;
   void * val;
 } key_val_t;
 
 typedef struct hash_table_s {
-  (list_t *) entries[];
+  (list_t *) entries[HASH_TABLE_SIZE];
 } hash_table_t;
 
 uint64_t       hash(char * str);
@@ -18,5 +20,7 @@ hash_table_t * hash_table_init(uint32_t size);
 void           hash_table_insert(hash_table_t * ht, void * item);
 void *         hash_table_lookup(hash_table_t * ht, void * item);
 uint32_t       string_distance(char *s1, char *s2);
+
+#undef HASH_TABLE_SIZE
 
 #endif  // LIB_HASH_
