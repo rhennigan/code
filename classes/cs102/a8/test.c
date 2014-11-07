96a2a46b645cfed60e416cc1cef3fe4b451f06a9
rhennigan <rhennigan@gmail.com>
2014-11-06 20:38:10 -0500

    auto-commit changes: Thu Nov  6 20:38:10 EST 2014

--- classes/cs102/a8/test.c
+++ classes/cs102/a8/test.c
@@ -4,6 +4,6 @@
 int main(int argc, char * argv[]) {
   char * str = argv[1];
   uint64_t h = hash(str);
-  printf("hash(%s) = %lud\n", str, h);
+  printf("hash(%s) = %lu\n", str, h);
   return 0;
 }
