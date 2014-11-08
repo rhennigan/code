cbb52b2f0a754fbca3727fd08dafafcb38a0816b
rhennigan <rhennigan@gmail.com>
2014-11-08 13:29:12 -0500

    auto-commit changes: Sat Nov  8 13:29:12 EST 2014

--- classes/cs102/a8/lib/bintree.h
+++ classes/cs102/a8/lib/bintree.h
@@ -15,17 +15,17 @@ typedef struct bintree_s {
   struct bintree_s * right;
 } bintree_t;
 
-size_t      bt_depth(bintree_t * tree);
-void        bt_dispose(bintree_t * tree);
-bool        bt_has_left(bintree_t * tree);
-bool        bt_has_right(bintree_t * tree);
+size_t      bt_depth(bintree_t * bt);
+void        bt_dispose(bintree_t * bt);
+bool        bt_has_left(bintree_t * bt);
+bool        bt_has_right(bintree_t * bt);
 bintree_t * bt_init();
-void        bt_insl(bintree_t * tree, void * data);
-void        bt_insr(bintree_t * tree, void * data);
-bool        bt_is_empty(bintree_t * tree);
-bool        bt_is_leaf(bintree_t * tree);
-bool        bt_is_node(bintree_t * tree);
-size_t      bt_leaf_count(bintree_t * tree);
-size_t      bt_node_count(bintree_t * tree);
+void        bt_insl(bintree_t * bt, void * data);
+void        bt_insr(bintree_t * bt, void * data);
+bool        bt_is_empty(bintree_t * bt);
+bool        bt_is_leaf(bintree_t * bt);
+bool        bt_is_node(bintree_t * bt);
+size_t      bt_leaf_count(bintree_t * bt);
+size_t      bt_node_count(bintree_t * bt);
 
 #endif  // LIB_BINTREE_H_
--- classes/cs102/a8/src/bintree.c
+++ classes/cs102/a8/src/bintree.c
@@ -3,13 +3,13 @@
 
 #include "../lib/bintree.h"
 
-size_t bt_depth(bintree_t * tree);
+size_t bt_depth(bintree_t * bt);
 
-void bt_dispose(bintree_t * tree);
+void bt_dispose(bintree_t * bt);
 
-bool bt_has_left(bintree_t * tree);
+bool bt_has_left(bintree_t * bt);
 
-bool bt_has_right(bintree_t * tree);
+bool bt_has_right(bintree_t * bt);
 
 bintree_t * bt_init() {
   bintree_t * bt = malloc(sizeof(bintree_t));
@@ -20,18 +20,19 @@ bintree_t * bt_init() {
   return bt;
 }
 
-void bt_insl(bintree_t * tree, void * data);
+void bt_insl(bintree_t * bt, void * data);
 
-void bt_insr(bintree_t * tree, void * data);
+void bt_insr(bintree_t * bt, void * data);
 
-bool bt_is_empty(bintree_t * tree) {
+bool bt_is_empty(bintree_t * bt) {
+  assert(bt != NULL);
   return bt->data == NULL && bt->left == NULL && bt->right == NULL;
 }
 
-bool bt_is_leaf(bintree_t * tree);
+bool bt_is_leaf(bintree_t * bt);
 
-bool bt_is_node(bintree_t * tree);
+bool bt_is_node(bintree_t * bt);
 
-size_t bt_leaf_count(bintree_t * tree);
+size_t bt_leaf_count(bintree_t * bt);
 
-size_t bt_node_count(bintree_t * tree);
+size_t bt_node_count(bintree_t * bt);
