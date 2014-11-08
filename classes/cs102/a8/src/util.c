1628047c28e419ab99a2c859aaba6444ce35e614
rhennigan <rhennigan@gmail.com>
2014-11-08 10:40:57 -0500

    auto-commit changes: Sat Nov  8 10:40:57 EST 2014

--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -104,5 +104,10 @@ char * match_str(char * s, hash_table_t * ht, char alts[][BUFSIZ]) {
 
   key_val_t kv = *(key_val_t*)addr;
   char * correct = kv.val.val;
+
+  double len1 = (double)strlen(s);
+  double len2 = (double)strlen(alts[minidx]);
+  double len = len1 > len2 ? len1 : len2;
+  double match = len / (len + (double)mindst);
   return correct;
 }
