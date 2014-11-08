2bd4f30510957b26e274ce69539c4a7046526d66
rhennigan <rhennigan@gmail.com>
2014-11-08 14:02:56 -0500

    auto-commit changes: Sat Nov  8 14:02:56 EST 2014

--- classes/cs102/a8/src/bintree.c
+++ classes/cs102/a8/src/bintree.c
@@ -93,6 +93,10 @@ size_t bt_node_count(bintree_t * bt) {
   if (bt == NULL) {
     return 0;
   } else if (bt_is_node(bt)) {
+    size_t lc = bt_node_count(bt_get_left(bt));
+    size_t rc = bt_node_count(bt_get_right(bt));
+    return lc + rc;
+  } else {
     return 0;
   }
 }
