df9d64d7706276197d9f6b7b0da5431d7eb6f2fb
rhennigan <rhennigan@gmail.com>
2014-11-08 09:30:16 -0500

    auto-commit changes: Sat Nov  8 09:30:16 EST 2014

--- classes/cs102/a8/lib/util.h
+++ classes/cs102/a8/lib/util.h
@@ -1,6 +1,8 @@
 #ifndef LIB_UTIL_
 #define LIB_UTIL_
 
+#include "./hash.h"
 
+hash_table_t * load_alternates(size_t size);
 
 #endif  // LIB_UTIL_
--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -1,3 +1,5 @@
 #include "../lib/util.h"
 
-
+hash_table_t * load_alternates(size_t size) {
+  
+}
