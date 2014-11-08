ff92557213a198c5216b4206ba1a89cb80527b15
rhennigan <rhennigan@gmail.com>
2014-11-08 11:08:00 -0500

    auto-commit changes: Sat Nov  8 11:08:00 EST 2014

--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -103,9 +103,9 @@ char * match_str(char * s, hash_table_t * ht, char alts[][BUFSIZ]) {
   key_val_t kv = *(key_val_t*)addr;
   char * correct = kv.val.val;
 
-  double len1 = (double)strlen(s)-1;
-  double len2 = (double)strlen(alts[minidx])-1;
-  double len = len1 > len2 ? len2 : len1;
+  double len1 = (double)strlen(s);
+  double len2 = (double)strlen(alts[minidx]);
+  double len = len1 < len2 ? len1 : len2;
   double match = len / (len + ((double)mindst * (double)mindst));
   uint32_t md = (int)(100*match*match);
   printf("matched %s (confidence: %d%%)\n", correct, md);
