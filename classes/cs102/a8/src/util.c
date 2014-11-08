48dac6dc2d350953184a0b21789eca0622c24436
rhennigan <rhennigan@gmail.com>
2014-11-08 12:52:18 -0500

    auto-commit changes: Sat Nov  8 12:52:18 EST 2014

--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -52,7 +52,7 @@ void load_text(const char * path, size_t len, char array[][BUFSIZ]) {
   fclose(file);
 }
 
-void load_ques(const char * path, size_t qcount, bool questions[][NUMQ]) {
+void load_answ(const char * path, size_t qcount, bool questions[][NUMQ]) {
   FILE * file = fopen(path, "r");
   char buff[BUFSIZ];
   for (uint32_t i = 0; i < NUMC; i++) {
