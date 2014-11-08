6f93641c1e6c4f2f22528434671826a1ef998977
rhennigan <rhennigan@gmail.com>
2014-11-08 13:24:51 -0500

    auto-commit changes: Sat Nov  8 13:24:51 EST 2014

--- classes/cs102/a8/src/bintree.c
+++ classes/cs102/a8/src/bintree.c
@@ -3,4 +3,24 @@
 
 #include "../lib/bintree.h"
 
+size_t bt_depth(bintree_t * tree);
 
+void bt_dispose(bintree_t * tree);
+
+bool bt_has_left(bintree_t * tree);
+
+bool bt_has_right(bintree_t * tree);
+
+bintree_t * bt_init();
+
+void bt_insl(bintree_t * tree, void * data);
+
+void bt_insr(bintree_t * tree, void * data);
+
+bool bt_is_leaf(bintree_t * tree);
+
+bool bt_is_node(bintree_t * tree);
+
+size_t bt_leaf_count(bintree_t * tree);
+
+size_t bt_node_count(bintree_t * tree);
