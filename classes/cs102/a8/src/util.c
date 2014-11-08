406467e141d09a5922d270e18029ca4f4c37cbfb
rhennigan <rhennigan@gmail.com>
2014-11-08 16:04:46 -0500

    auto-commit changes: Sat Nov  8 16:04:46 EST 2014

--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -181,16 +181,16 @@ void div_tree(bintree_t * bt) {
     }
   }
 
-  printf("\n\n--------------------\n");
-  printf("total = ");
-  for (uint32_t i = 0; i < NUMQ; i++)
-    printf(" %d", total[i]);
+  /* printf("\n\n--------------------\n"); */
+  /* printf("total = "); */
+  /* for (uint32_t i = 0; i < NUMQ; i++) */
+  /*   printf(" %d", total[i]); */
 
   printf("\n\n--------------------\n");
-  printf("mid = %d\n", mid);
-  printf("mindist = %d\n", mindist);
-  printf("splitby = %d\n", splitby);
-  printf("total[splitby] = %d\n", total[splitby]);
+  /* printf("mid = %d\n", mid); */
+  /* printf("mindist = %d\n", mindist); */
+  /* printf("splitby = %d\n", splitby); */
+  /* printf("total[splitby] = %d\n", total[splitby]); */
 
   list_t * cvecs_l = NULL;
   list_t * cvecs_r = NULL;
