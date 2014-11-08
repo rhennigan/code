c0ed6c5dd9e946247ba719e21c4d52795a5d3c2d
rhennigan <rhennigan@gmail.com>
2014-11-08 13:40:30 -0500

    auto-commit changes: Sat Nov  8 13:40:30 EST 2014

--- classes/cs102/a8/src/bintree.c
+++ classes/cs102/a8/src/bintree.c
@@ -33,13 +33,13 @@ bintree_t * bt_init() {
 
 void bt_insl(bintree_t * bt, void * data) {
   assert(bt != NULL);
+  bt->left = data;
 }
 
 void bt_insr(bintree_t * bt, void * data);
 
 bool bt_is_empty(bintree_t * bt) {
-  assert(bt != NULL);
-  return bt->data == NULL && bt->left == NULL && bt->right == NULL;
+  return !bt_has_data(bt) && !bt_has_left(bt) && !bt_has_right(bt);
 }
 
 bool bt_is_leaf(bintree_t * bt) {
