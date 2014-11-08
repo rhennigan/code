52caab790bef099ddf0b0d94b2433615669f9f4e
rhennigan <rhennigan@gmail.com>
2014-11-08 09:59:00 -0500

    auto-commit changes: Sat Nov  8 09:59:00 EST 2014

--- classes/cs102/a8/lib/util.h
+++ classes/cs102/a8/lib/util.h
@@ -11,6 +11,6 @@ void           print_int(void * addr);
 void           print_kv(void * addr);
 bool           equal(void * a, void * b);
 void           dbg_alts(hash_table_t * ht, char alts[][BUFSIZ], size_t a_size);
-char *         match_str(char * s, hash_table_t * ht, char ** alts, size_t as);
+char *         match_str(char * s, hash_table_t * ht, char alts[][BUFSIZ], size_t as);
 
 #endif  // LIB_UTIL_
