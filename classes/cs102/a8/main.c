1f7e228bd6a49c2ecab4ddd42959c06e3eb8b162
rhennigan <rhennigan@gmail.com>
2014-11-08 17:35:40 -0500

    auto-commit changes: Sat Nov  8 17:35:40 EST 2014

--- classes/cs102/a8/main.c
+++ classes/cs102/a8/main.c
@@ -39,7 +39,7 @@ void get_help(hash_table_t * ht) {
     char * q = questions[i];
     char * a = q_answers[cidx][i+1] ? "YES" : "NO";
     printf("%s = %s\n", q, a);
-    if (i % 21 == 0) {
+    if (i % 21 == 20) {
       WAIT();
       printf("\n");
       printf("%s Facts\n", mst);
