87b796f14df32be28c95bad6c533a876702df448
rhennigan <rhennigan@gmail.com>
2014-11-08 13:34:20 -0500

    auto-commit changes: Sat Nov  8 13:34:20 EST 2014

--- classes/cs102/a8/src/bintree.c
+++ classes/cs102/a8/src/bintree.c
@@ -7,6 +7,11 @@ size_t bt_depth(bintree_t * bt);
 
 void bt_dispose(bintree_t * bt);
 
+bool bt_has_data(bintree_t * bt) {
+  assert(bt != NULL);
+  return bt->data != NULL;
+}
+
 bool bt_has_left(bintree_t * bt) {
   assert(bt != NULL);
   return bt->left != NULL;
@@ -39,7 +44,9 @@ bool bt_is_leaf(bintree_t * bt) {
   if (bt_is_empty(bt)) return false;
 }
 
-bool bt_is_node(bintree_t * bt);
+bool bt_is_node(bintree_t * bt) {
+  return bt_has_data(bt) && !bt_has_left(bt) && !bt_has_right(bt);
+}
 
 size_t bt_leaf_count(bintree_t * bt);
 
