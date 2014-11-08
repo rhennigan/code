d174c0f57f277135f58761fbc6d46e9e46c7003f
rhennigan <rhennigan@gmail.com>
2014-11-08 09:31:17 -0500

    auto-commit changes: Sat Nov  8 09:31:17 EST 2014

--- classes/cs102/a8/src/util.c
+++ classes/cs102/a8/src/util.c
@@ -1,5 +1,41 @@
+#include <stdio.h>
 #include "../lib/util.h"
 
 hash_table_t * load_alternates(size_t size) {
-  
+  FILE *   alts_file;
+  hash_table_t * hash_table = hash_table_init(size);
+
+  alts_file = fopen("data/alternates.csv", "r");
+
+  for (i = 0; i < NUMA; i++) {
+    fgets(buffer, BUFSIZ, alts_file);
+    char * key = malloc(BUFSIZ);
+    char * val = malloc(BUFSIZ);
+    memset(key, '\0', BUFSIZ);
+    memset(val, '\0', BUFSIZ);
+    for (j = 0; j < BUFSIZ; j++) {
+      if (buffer[j] == ',') {
+        key[j] = '\0';
+        break;
+      }
+      key[j] = buffer[j];
+    }
+    for (k = j + 1; k < BUFSIZ; k++) {
+      if (buffer[k] == '\n') {
+        val[k-j-1] = '\0';
+        break;
+      }
+      val[k-j-1] = buffer[k];
+      if (buffer[k] == '\0') break;
+    }
+    snprintf(alternate[i], BUFSIZ, "%s", key);
+    printf("key = %s (%lu), val = %s (%lu)\n",
+           key, strlen(key), val, strlen(val));
+    key_val_t * kv = make_kv(key, strlen(key)+1, val, strlen(val)+1);
+    hash_table_insert(hash_table, kv);
+    free(kv);
+  }
+
+  fclose(alts_file);
+  return hash_table;
 }
