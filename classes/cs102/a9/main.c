1407a5733187f02d4c8bb9d61892d541e3b829b2
rhennigan <rhennigan@gmail.com>
2014-11-15 15:49:17 -0500

    auto-commit changes: Sat Nov 15 15:49:17 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -3,11 +3,15 @@
 
 #include "lib/bst.h"
 
+#define ARRSIZ 15
+
 int32_t intcmp(void * a, void * b) {
   return *(int32_t*)a - *(int32_t*)b;
 }
 
 int main(int argc, char *argv[]) {
   bst_t * bst = bst_init();
+  int32_t arr[ARRSIZ];
+  for (int32_t i = 0; i < ARRSIZ; i++) arr[i] = nrand48();
   return 0;
 }
