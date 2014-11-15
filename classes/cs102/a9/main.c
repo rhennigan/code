125953b55352aee11ecca338874e25f35892b7b3
rhennigan <rhennigan@gmail.com>
2014-11-15 15:48:16 -0500

    auto-commit changes: Sat Nov 15 15:48:16 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -3,7 +3,9 @@
 
 #include "lib/bst.h"
 
-int32_t intcmp(void * a, void * b);
+int32_t intcmp(void * a, void * b) {
+  return *(int32_t*)a - *(int32_t*)b;
+}
 
 int main(int argc, char *argv[]) {
   bst_t * bst = bst_init();
