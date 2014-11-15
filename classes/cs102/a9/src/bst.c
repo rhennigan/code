fc5b6e25279e6b6a89df28688b3a3ee3ee94bfb5
rhennigan <rhennigan@gmail.com>
2014-11-15 13:59:35 -0500

    auto-commit changes: Sat Nov 15 13:59:35 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -4,3 +4,20 @@
 #include "../lib/bst.h"
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+size_t  bst_depth(bst_t * bst);
+
+void    bst_dispose(bst_t * bst);
+
+void    bst_flatten(bst_t * bst, list_t ** list);
+
+bst_t * bst_init();
+
+bst_t * bst_insert(bst_t * bst, void * data, cmp_f cmp);
+
+void    bst_print(bst_t * bst);
+
+void    bst_remove(bst_t * bst, void * data, cmp_f cmp);
+
+void *  bst_search(bst_t * bst, void * data, void * result, cmp_f cmp);
+
