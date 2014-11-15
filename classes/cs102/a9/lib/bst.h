f5c49c1c4344227c9d35ba851386dfaa85793e07
rhennigan <rhennigan@gmail.com>
2014-11-15 15:21:32 -0500

    auto-commit changes: Sat Nov 15 15:21:32 EST 2014

--- classes/cs102/a9/lib/bst.h
+++ classes/cs102/a9/lib/bst.h
@@ -22,6 +22,7 @@ typedef struct bst_s {
 } bst_t;
 
 typedef int32_t (*cmp_f)(void * a, void * b);
+typedef void (*pr_fun)(void * data);
 
 size_t  bst_depth(bst_t * bst);
 void    bst_dispose(bst_t * bst);
@@ -29,7 +30,7 @@ void    bst_dump(bst_t * bst, order_t order);
 void    bst_flatten(bst_t * bst, list_t ** list, order_t order);
 bst_t * bst_init();
 bst_t * bst_insert(bst_t * bst, void * data, cmp_f cmp);
-void    bst_print(bst_t * bst, void (*pr)(void * data));
+void    bst_print(bst_t * bst, pr_f pf);
 void    bst_remove(bst_t * bst, void * data, cmp_f cmp);
 void *  bst_search(bst_t * bst, void * data, void * result, cmp_f cmp);
 
