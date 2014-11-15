b153c85edf7f35648ec9353747fb919b3607cadb
rhennigan <rhennigan@gmail.com>
2014-11-15 13:31:59 -0500

    auto-commit changes: Sat Nov 15 13:31:59 EST 2014

--- classes/cs102/a9/lib/bst.h
+++ classes/cs102/a9/lib/bst.h
@@ -8,10 +8,11 @@
 
 typedef struct bst_s {
   struct bst_s *left, *parent, *right;
-  void * data;
+  void *data;
   size_t depth;
 } bst_t;
 
-
+size_t bst_depth(bst_t * bst);
+void   bst_dispose(bst_t * bst);
 
 #endif  // LIB_BINTREE_H_
