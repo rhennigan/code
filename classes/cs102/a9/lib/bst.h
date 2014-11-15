4c112626c6b6e3c91200387c65c5329c853b464a
rhennigan <rhennigan@gmail.com>
2014-11-15 13:51:25 -0500

    auto-commit changes: Sat Nov 15 13:51:25 EST 2014

--- classes/cs102/a9/lib/bst.h
+++ classes/cs102/a9/lib/bst.h
@@ -16,13 +16,13 @@ typedef struct bst_s {
   size_t         depth;
 } bst_t;
 
-typedef int32_t (*cmp_fun)(void * a, void * b);
+typedef int32_t (*cmp_f)(void * a, void * b);
 
 size_t  bst_depth(bst_t * bst);
 void    bst_dispose(bst_t * bst);
 void    bst_flatten(bst_t * bst, list_t ** list);
 bst_t * bst_init();
-bst_t * bst_insert(bst_t * bst, void * data, cmp_fun cmp);
+bst_t * bst_insert(bst_t * bst, void * data, cmp_f cmp);
 void    bst_print(bst_t * bst);
 
 #endif  // LIB_BINTREE_H_
