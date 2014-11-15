a8f81cf0924a9f7771cdd722d7d62f1569db928b
rhennigan <rhennigan@gmail.com>
2014-11-15 16:13:46 -0500

    auto-commit changes: Sat Nov 15 16:13:46 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -22,15 +22,17 @@ void print_node(bst_t * node) {
   }
 }
 
+size_t force_depth(bst_t * bst);
+
 int main(int argc, char *argv[]) {
   bst_t * bst = bst_init();
   int64_t arr[ARRSIZ];
   for (int32_t i = 0; i < ARRSIZ; i++) {
     arr[i] = rand();
-    printf("%lu\n", arr[i]);
     bst_insert(bst, &arr[i], &intcmp);
   }
 
+  force_depth(bst);
   bst_print(bst, &print_node);
   return 0;
 }
