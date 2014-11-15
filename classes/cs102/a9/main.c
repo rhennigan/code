be722043daac6774503bab257962fcfc5e8d2ede
rhennigan <rhennigan@gmail.com>
2014-11-15 15:46:11 -0500

    auto-commit changes: Sat Nov 15 15:46:11 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -1,3 +1,9 @@
 // main.c - testing bst.h and bst.c
 // Copyright (C) 2014 Richard Hennigan
 
+#include "lib/bst.h"
+
+int main(int argc, char *argv[]) {
+  bst_t * bst = bst_init();
+  return 0;
+}
