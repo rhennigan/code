1caa53cec3d669467804e80d5eba263508432b46
rhennigan <rhennigan@gmail.com>
2014-11-15 17:31:26 -0500

    auto-commit changes: Sat Nov 15 17:31:26 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -256,7 +256,7 @@ void show_tree(bst_t *root, struct trunk *prev) {
     this_disp.str = B_TL""B_HR""B_HR;
     prev_str = "   "B_VT;
   } else {
-    this_disp.str = "`--";
+    this_disp.str = "`"B_HR""B_HR;
     prev->str = prev_str;
   }
 
