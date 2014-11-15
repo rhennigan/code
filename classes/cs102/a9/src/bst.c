d25a29ed01267b1a9580ac9eb72d66f07e01d56c
rhennigan <rhennigan@gmail.com>
2014-11-15 17:40:37 -0500

    auto-commit changes: Sat Nov 15 17:40:37 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -246,7 +246,7 @@ void show_trunks(struct trunk * p) {
 
 void show_tree(bst_t *root, struct trunk *prev) {
   if (root == NULL) return;
-  struct trunk this_disp = { prev, "    " };
+  struct trunk this_disp = { prev, "****" };
   char *prev_str = this_disp.str;
   show_tree(get_left(root), &this_disp);
 
@@ -264,7 +264,7 @@ void show_tree(bst_t *root, struct trunk *prev) {
   printf("%d\n", *(int*)get_data(root));
 
   if (prev) prev->str = prev_str;
-  this_disp.str = "   "B_VT;
+  this_disp.str = "    "B_VT;
 
   show_tree(get_right(root), &this_disp);
   if (!prev) puts("");
