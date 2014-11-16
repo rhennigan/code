d9adc4334da66e74c8314286b2bd68e5c0364c53
rhennigan <rhennigan@gmail.com>
2014-11-15 19:44:39 -0500

    auto-commit changes: Sat Nov 15 19:44:39 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -176,10 +176,13 @@ void rotate_right(bst_t ** bst) {
 
 void bst_balance(bst_t * bst) {
   if (bst == NULL) return;
+  rotate_left(&bst);
   int32_t lh = (int32_t)bst_height(get_left(bst));
   int32_t rh = (int32_t)bst_height(get_right(bst));
-  while (rh - lh > 1) rotate_left(&bst);
-  while (lh - rh > 0) rotate_right(&bst);
+  printf("left = %d, right = %d\n", lh, rh);
+  
+  /* while (rh - lh > 1) rotate_left(&bst); */
+  /* while (lh - rh > 0) rotate_right(&bst); */
 }
 
 size_t bst_height(bst_t * bst) {
