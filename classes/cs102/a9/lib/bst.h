04ee5a11173de77eb2aa59bfde99bc051bbf08e3
rhennigan <rhennigan@gmail.com>
2014-11-15 19:07:44 -0500

    auto-commit changes: Sat Nov 15 19:07:44 EST 2014

--- classes/cs102/a9/lib/bst.h
+++ classes/cs102/a9/lib/bst.h
@@ -36,9 +36,9 @@ void    bst_flatten(bst_t * bst, list_t ** list, order_t order);
 size_t  force_depth(bst_t * bst);
 bst_t * bst_init();
 void    bst_insert(bst_t * bst, void * data, cmp_fun cmp);
+void    bst_print(bst_t * bst, trunk_t * prev, pr_fun pf);
 void    bst_remove(bst_t * bst, void * data, cmp_fun cmp);
 void *  bst_search(bst_t * bst, void * data, void * result, cmp_fun cmp);
 void    bst_update_depth(bst_t * bst);
-void    bst_print(bst_t * bst, trunk_t * prev, pr_fun pf);
 
 #endif  // LIB_BINTREE_H_
