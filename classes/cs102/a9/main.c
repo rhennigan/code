45ea708d3d1582f222b1227111ffb1cf58631b3f
rhennigan <rhennigan@gmail.com>
2014-11-15 19:30:23 -0500

    auto-commit changes: Sat Nov 15 19:30:23 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -15,7 +15,7 @@ int32_t intcmp(void * a, void * b) {
 
 void pf(bst_t * bst) {
   if (bst == NULL || bst->data == NULL) return;
-  printf("(%lu, %d)", bst->depth, *(int32_t*)bst->data);
+  printf("(%lu, %d)", bst_height(bst), *(int32_t*)bst->data);
 }
 
 int main(int argc, char *argv[]) {
