08b9c19b096dd00994ceed7c415fa57ca6cb46c7
rhennigan <rhennigan@gmail.com>
2014-11-15 19:53:51 -0500

    auto-commit changes: Sat Nov 15 19:53:51 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -181,17 +181,17 @@ void pf(bst_t * bst) {
 
 
 
-void bst_balance(bst_t * bst) {
-  if (bst == NULL) return;
-  int32_t lh = (int32_t)bst_height(get_left(bst));
-  int32_t rh = (int32_t)bst_height(get_right(bst));
+void bst_balance(bst_t ** bst) {
+  if (*bst == NULL) return;
+  int32_t lh = (int32_t)bst_height(get_left(*bst));
+  int32_t rh = (int32_t)bst_height(get_right(*bst));
   printf("left = %d, right = %d\n", lh, rh);
   printf("rh - lh = %d\n", rh - lh);
   printf("lh - rh = %d\n", lh - rh);
   while (lh - rh > 0) {
-    rotate_right(&bst);
-    lh = (int32_t)bst_height(get_left(bst));
-    rh = (int32_t)bst_height(get_right(bst));
+    rotate_right(bst);
+    lh = (int32_t)bst_height(get_left(*bst));
+    rh = (int32_t)bst_height(get_right(*bst));
     printf("after rotate_right\n");
     printf("left = %d, right = %d\n", lh, rh);
     printf("rh - lh = %d\n", rh - lh);
