994bbc72db184f93402705056db4f5f3fc115afa
rhennigan <rhennigan@gmail.com>
2014-11-15 22:58:26 -0500

    auto-commit changes: Sat Nov 15 22:58:26 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -174,8 +174,8 @@ bst_t * rotate_left(bst_t * bst) {
 /* } */
 
 static inline int32_t bal(bst_t * bst) {
-  int32_t llc = (int32_t)bst_height(get_left(bst));
-  int32_t rlc = (int32_t)bst_height(get_right(bst));
+  int32_t llc = (int32_t)bst_node_count(get_left(bst));
+  int32_t rlc = (int32_t)bst_node_count(get_right(bst));
   return rlc - llc;
 }
 
@@ -185,7 +185,8 @@ static inline int32_t bal(bst_t * bst) {
 
 void pf(bst_t * bst) {
   if (bst == NULL || bst->data == NULL) return;
-  printf(" %d", *(int32_t*)bst->data);
+  /* printf(" %d", *(int32_t*)bst->data); */
+  printf(" %lu", bst_height(bst));
 }
 
 static bst_t * balance3(bst_t * bst) {
