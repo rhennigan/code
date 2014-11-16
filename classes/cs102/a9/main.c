67e2278a529a67da1fa3a24f21e8e2f87aee0a79
rhennigan <rhennigan@gmail.com>
2014-11-15 23:04:50 -0500

    auto-commit changes: Sat Nov 15 23:04:50 EST 2014

--- classes/cs102/a9/main.c
+++ classes/cs102/a9/main.c
@@ -38,16 +38,12 @@ int main(int argc, char *argv[]) {
   printf("\n---------------------------------------------\n");
   bst_print(bst, NULL, &pf);
 
-  printf("\n---------------------------------------------\n");
-  bst = bst_balance(bst);
-  printf("\n---------------------------------------------\n");
-  bst_print(bst, NULL, &pf);
-
-  printf("\n---------------------------------------------\n");
-  bst = bst_balance(bst);
-  printf("\n---------------------------------------------\n");
-  bst_print(bst, NULL, &pf);
-
-  
+  printf("BALANCING\n");
+  for (int i = 0; i < 10; i++) {
+    printf("\n---------------------------------------------\n");
+    bst = bst_balance(bst);
+    printf("\n---------------------------------------------\n");
+    bst_print(bst, NULL, &pf);
+  }
   return 0;
 }
