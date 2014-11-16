952de4738d23b2bc8126111ebafcfd55a9ec34ee
rhennigan <rhennigan@gmail.com>
2014-11-15 21:22:58 -0500

    auto-commit changes: Sat Nov 15 21:22:58 EST 2014

--- classes/cs102/a9/src/bst.c
+++ classes/cs102/a9/src/bst.c
@@ -182,14 +182,14 @@ void pf(bst_t * bst) {
 bst_t * bst_balance(bst_t * bst) {
   printf("balancing %p\n", bst);
   if (bst == NULL || is_leaf(bst)) return bst;
-  if (get_left(bst) != NULL && ABS(bal(get_left(bst))) > 1)
+  if (/* get_left(bst) != NULL &&  */ABS(bal(get_left(bst))) > 1)
     set_left(bst, bst_balance(get_left(bst)));
-  if (get_right(bst) != NULL && ABS(bal(get_right(bst))) > 1)
+  if (/* get_right(bst) != NULL &&  */ABS(bal(get_right(bst))) > 1)
     set_right(bst, bst_balance(get_right(bst)));
   while (bal(bst) > 1) {
     printf("\n---------------------------------------------\n");
     printf("balance = %d\n", bal(bst));
-    printf("ROTATING:\n");
+    printf("ROTATING LEFT:\n");
     bst_print(bst, NULL, &pf);
     bst = rotate_left(bst);
     printf("RESULTS:\n");
@@ -199,7 +199,7 @@ bst_t * bst_balance(bst_t * bst) {
   while (bal(bst) < -1) {
     printf("\n---------------------------------------------\n");
     printf("balance = %d\n", bal(bst));
-    printf("ROTATING:\n");
+    printf("ROTATING RIGHT:\n");
     bst_print(bst, NULL, &pf);
     bst = rotate_right(bst);
     printf("RESULTS:\n");
