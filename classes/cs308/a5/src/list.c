8345c357cda6a038f4ec673dcf6605b54db6d794
rhennigan <rhennigan@gmail.com>
2014-11-25 18:21:16 -0500

    auto-commit changes: Tue Nov 25 18:21:16 EST 2014

--- classes/cs308/a5/src/list.c
+++ classes/cs308/a5/src/list.c
@@ -177,8 +177,8 @@ static inline list_t * merge(list_t * xxs, list_t * yys, cmp_fun lt) {
   }  // end if (xs == NULL && ys == NULL)
 }
 
-static inline list_t * merge_sort(list_t * list, cmp_fun lt,
-                                  list_t * left, list_t * right) {
+static inline list_t * merge_sort(
+    list_t * list, cmp_fun lt, list_t * left, list_t * right) {
   if (list == NULL) {
     return NULL;
   } else {  // (list != NULL)
