faf072fa6b0a7140a208696d57d0488a72217690
rhennigan <rhennigan@gmail.com>
2014-11-25 13:49:50 -0500

    auto-commit changes: Tue Nov 25 13:49:50 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -1,5 +1,8 @@
 #include "lib/list.h"
 
 int main(int argc, char *argv[]) {
+  int array[100];
+  for (i = 0; i < 100; i++) array[i] = i * i;
+  list_t * list = list_fromarray(array, sizeof(int), 100);
   return 0;
 }
