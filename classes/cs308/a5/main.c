dea58b3900ed1e9845d67d4516140d9f424c7b1d
rhennigan <rhennigan@gmail.com>
2014-11-25 13:52:54 -0500

    auto-commit changes: Tue Nov 25 13:52:54 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -1,13 +1,13 @@
 #include "lib/list.h"
 
 void pl(void * head) {
-  printf(" %d", *(int*)head);
+  printf(" %f", *(double*)head);
 }
 
 int main(int argc, char *argv[]) {
-  int array[100];
-  for (int i = 0; i < 100; i++) array[i] = i * i;
-  list_t * list = list_fromarray(array, sizeof(int), 100);
+  double array[100];
+  for (int i = 0; i < 100; i++) array[i] = (double)i;
+  list_t * list = list_fromarray(array, sizeof(double), 100);
   list_iter(list, &pl);
   return 0;
 }
