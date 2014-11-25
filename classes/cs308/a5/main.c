d35b3ab08c1ec99e8db86a4bc9478bf0d8f29eac
rhennigan <rhennigan@gmail.com>
2014-11-25 13:55:58 -0500

    auto-commit changes: Tue Nov 25 13:55:58 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -1,14 +1,18 @@
 #include "lib/list.h"
 
-void pl(void * head) {
+void pint(void * head) {
+  printf(" %d", *(int*)head);
+}
+
+void pdouble(void * head) {
   printf(" %f", *(double*)head);
 }
 
 int main(int argc, char *argv[]) {
-  double array[100];
-  for (int i = 0; i < 100; i++) array[i] = (double)i;
-  list_t * list = list_fromarray(array, sizeof(double), 100);
-  list_iter(list, &pl);
+  int array[100];
+  for (int i = 0; i < 100; i++) array[i] = (int)i;
+  list_t * list = list_fromarray(array, sizeof(int), 100);
+  list_iter(list, &pint);
   printf("\n\nsizeof(int) = %lu\nsizeof(double) = %lu",
          sizeof(int), sizeof(double));
   return 0;
