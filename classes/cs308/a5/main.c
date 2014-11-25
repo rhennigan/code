b548862ddbcb446a89463e1c6cb4426325e7779c
rhennigan <rhennigan@gmail.com>
2014-11-25 13:53:56 -0500

    auto-commit changes: Tue Nov 25 13:53:56 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -9,5 +9,7 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < 100; i++) array[i] = (double)i;
   list_t * list = list_fromarray(array, sizeof(double), 100);
   list_iter(list, &pl);
+  printf("\n\nsizeof(int) = %lul\nsizeof(double) = %d",
+         sizeof(int), sizeof(double));
   return 0;
 }
