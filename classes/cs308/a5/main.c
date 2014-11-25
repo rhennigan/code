f8dd4c3ebf568acffd9d3bbe86361880cc8c20f3
rhennigan <rhennigan@gmail.com>
2014-11-25 18:55:13 -0500

    auto-commit changes: Tue Nov 25 18:55:13 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -14,18 +14,21 @@ bool intlt(void * a, void * b) {
   return (*(int*)a) <= (*(int*)b);
 }
 
+#define COUNT 100
+
 int main(int argc, char *argv[]) {
   printf("\n\n\n\n\n\n\n\n\n\n\n");
-  int array[10];
-  for (int i = 0; i < 10; i++)
+  int array[COUNT];
+  for (int i = 0; i < COUNT; i++)
     array[i] = rand() % 100;
-  list_t * list = list_fromarray(array, sizeof(int), 10);
+  list_t * list = list_fromarray(array, sizeof(int), COUNT);
   list_iter(list, &pint);
   printf("\n\nsizeof(int) = %lu\nsizeof(double) = %lu",
          sizeof(int), sizeof(double));
   printf("\n\n");
   list_dump(list);
 
+  printf("\n\n\n\n\n\n\n\n\n\n\n");
   list_t * sorted = list_sort(list, &intlt);
   list_iter(sorted, &pint);
   list_dump(sorted);
