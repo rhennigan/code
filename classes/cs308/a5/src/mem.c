3f52fbd746203e5e7b4917dae683e8ddc24e164e
rhennigan <rhennigan@gmail.com>
2014-11-25 21:41:07 -0500

    auto-commit changes: Tue Nov 25 21:41:07 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -34,18 +34,4 @@ void print_mem_config() {
   printf("MIN_ALLOC_BYTES     = %lu\n", MIN_ALLOC_BYTES);
   printf("MAX_POOL_SIZE_WORDS = %lu\n", MAX_POOL_SIZE_WORDS);
   printf("MIN_ALLOC_WORDS     = %lu\n", MIN_ALLOC_WORDS);
-  printf("
-/******************************************************************************/
-/* CONFIG OPTIONS                                                             */
-/******************************************************************************/
-MAX_POOL_SIZE_MBYTES  (1024ul)
-MIN_ALLOC_BYTES       (32ul)
-MAX_HISTORY_LENGTH    (1000ul)
-
-/******************************************************************************/
-MAX_POOL_SIZE_BYTES   (1048576ul * MAX_POOL_SIZE_MBYTES)
-WORD_SIZE_BYTES       (sizeof(void *))
-MAX_POOL_SIZE_WORDS   (MAX_POOL_SIZE_BYTES / WORD_SIZE_BYTES)
-MIN_ALLOC_WORDS       (MIN_ALLOC_BYTES / WORD_SIZE_BYTES)
-");
 }
