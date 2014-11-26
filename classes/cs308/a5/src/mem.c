6d5b5ceb4937976b71841732e8220d6d2fc91454
rhennigan <rhennigan@gmail.com>
2014-11-26 17:47:35 -0500

    auto-commit changes: Wed Nov 26 17:47:35 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -118,10 +118,10 @@ void split_block(mem_block_t * block, request_t * request) {
     prev_list_node->tail = alloc_list_node;                        // 1
   }
 
-  /************ Update block pointers ************/
+  /**************** Update block pointers ***************/
   if (prev_list_node != NULL) {
     mem_block_t * prev_block = (mem_block_t *)prev_list_node->head;
-    prev_block->next = alloc_list_node;
+    prev_block->next = alloc_list_node;                            // 1
   }
 
   alloc_block->prev    = block->prev;
