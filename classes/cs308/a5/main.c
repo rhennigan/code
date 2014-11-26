cc01172542ac06fbd4e954da89dd0e50d4e0789a
rhennigan <rhennigan@gmail.com>
2014-11-25 19:21:00 -0500

    auto-commit changes: Tue Nov 25 19:21:00 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "lib/list.h"
 
 #define MAX_POOL_SIZE (1048576)
@@ -6,7 +7,12 @@
 typedef enum { FIRST_FIT, BEST_FIT, BUDDY_SYSTEM } policy_t;
 
 int main(int argc, char *argv[]) {
+  /* LOAD ARGUMENTS */
   assert(argc == 4);
+  policy_t policy;
+  if (strcmp(argv[1], "first") == 0) {
+    policy = FIRST_FIT;
+  };
   char * policy_str = argv[1];
   size_t pool_size = atoi(argv[2]);
   char * req_file = argv[3];
