8693b5bb8225546ab42cd452e3d141e34bcb906f
rhennigan <rhennigan@gmail.com>
2014-11-25 21:40:05 -0500

    auto-commit changes: Tue Nov 25 21:40:05 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -12,6 +12,13 @@ policy_t     policy;
 bytes_t      pool_size;
 
 /******************************************************************************/
+static inline void hline() {
+  printf("/");
+  for (int i = 0; i < 78; i++)
+    printf("*");
+  printf("/");
+}
+
 void print_usage(char * name) {
   printf("error reading arguments\n");
   printf("usage:\n");
@@ -27,4 +34,18 @@ void print_mem_config() {
   printf("MIN_ALLOC_BYTES     = %lu\n", MIN_ALLOC_BYTES);
   printf("MAX_POOL_SIZE_WORDS = %lu\n", MAX_POOL_SIZE_WORDS);
   printf("MIN_ALLOC_WORDS     = %lu\n", MIN_ALLOC_WORDS);
+  printf("
+/******************************************************************************/
+/* CONFIG OPTIONS                                                             */
+/******************************************************************************/
+MAX_POOL_SIZE_MBYTES  (1024ul)
+MIN_ALLOC_BYTES       (32ul)
+MAX_HISTORY_LENGTH    (1000ul)
+
+/******************************************************************************/
+MAX_POOL_SIZE_BYTES   (1048576ul * MAX_POOL_SIZE_MBYTES)
+WORD_SIZE_BYTES       (sizeof(void *))
+MAX_POOL_SIZE_WORDS   (MAX_POOL_SIZE_BYTES / WORD_SIZE_BYTES)
+MIN_ALLOC_WORDS       (MIN_ALLOC_BYTES / WORD_SIZE_BYTES)
+");
 }
