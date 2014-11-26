de702cedc36f29814934d0ac0b091d2a158f9f91
rhennigan <rhennigan@gmail.com>
2014-11-25 22:46:29 -0500

    auto-commit changes: Tue Nov 25 22:46:29 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -32,5 +32,7 @@ void print_mem_config() {
   printf(" MAX_HISTORY_LENGTH   = %lu\n", MAX_HISTORY_LENGTH);
   printf("                           \n");
   printf(" MAX_POOL_SIZE_BYTES  = %lu\n", MAX_POOL_SIZE_BYTES);
+  printf(" WORD_SIZE_BYTES      = %lu\n", WORD_SIZE_BYTES);
+  printf(" MAX_POOL_SIZE_WORDS  = %lu\n", MAX_POOL_SIZE_WORDS);
   printf(" MIN_ALLOC_WORDS      = %lu\n", MIN_ALLOC_WORDS);
 }
