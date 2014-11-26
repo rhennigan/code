5acbd2647c5e32ceff3b1fbc45d0ddb38130484b
rhennigan <rhennigan@gmail.com>
2014-11-25 22:39:20 -0500

    auto-commit changes: Tue Nov 25 22:39:20 EST 2014

--- classes/cs308/a5/lib/mem.h
+++ classes/cs308/a5/lib/mem.h
@@ -5,6 +5,7 @@
 
 #include <string.h>
 #include "./list.h"
+#include "./io.h"
 
 /******************************************************************************/
 /* CONFIG OPTIONS                                                             */
--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -12,65 +12,8 @@ policy_t     policy;
 bytes_t      pool_size;
 
 /******************************************************************************/
-/* UNICODE CHARACTERS FOR BOX DRAWING                                         */
-/******************************************************************************/
-
-#define B_HR "\u2500"  // horizontal bar
-#define B_TL "\u250C"  // top-left box corner
-#define B_TR "\u2510"  // top-right box corner
-#define B_BL "\u2514"  // bottom-left box corner
-#define B_BR "\u2518"  // bottom-right box corner
-#define B_VT "\u2502"  // vertical bar
-
-/******************************************************************************/
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-
-static inline void tline(size_t width, size_t pad) {
-  for (size_t i = 0; i < pad; i++)
-    printf(" ");
-  printf("%s", B_TL);
-  for (size_t i = 0; i < width-2; i++)
-    printf("%s", B_HR);
-  printf("%s\n", B_TR);
-}
-
-static inline void bline(size_t width, size_t pad) {
-  for (size_t i = 0; i < pad; i++)
-    printf(" ");
-  printf("%s", B_BL);
-  for (size_t i = 0; i < width-2; i++)
-    printf("%s", B_HR);
-  printf("%s\n", B_BR);
-}
-
-static inline void print_boxed(const char * label, size_t width, size_t pad) {
-  char label_str[86];
-  size_t inspadl = (width-strlen(label)-2) / 2;
-  size_t inspadr = inspadl + (width-strlen(label)-2) % 2;
-  printf("inspadl = %lu\n", inspadl);
-  printf("inspadr = %lu\n", inspadr);
-  size_t i = 0;
-  for (size_t j = 0; j < pad; j++)
-    label_str[i++] = ' ';
-  label_str[i++] = 0xe2;
-  label_str[i++] = 0x94;
-  label_str[i++] = 0x82;
-  for (size_t j = 0; j < inspadl; j++)
-    label_str[i++] = '*';
-  for (size_t j = 0; j < strlen(label); j++)
-    label_str[i++] = label[j];
-  for (size_t j = 0; j < inspadr; j++)
-    label_str[i++] = '*';
-  label_str[i++] = 0xe2;
-  label_str[i++] = 0x94;
-  label_str[i++] = 0x82;
-  label_str[i++] = '\n';
-  label_str[i++] = '\0';
-  tline(width, pad);
-  printf("%s", label_str);
-  bline(width, pad);
-}
 
 void print_usage(char * name) {
   printf("error reading arguments\n");
