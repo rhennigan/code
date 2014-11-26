b939992cbb20d2a2a1214165e233c7e0784313f7
rhennigan <rhennigan@gmail.com>
2014-11-25 23:14:08 -0500

    auto-commit changes: Tue Nov 25 23:14:08 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -41,6 +41,5 @@ void print_output_header() {
   char label[80];
   char * mp = "MANAGEMENT POLICY";
   char * ps = "POOL SIZE";
-  snprintf(label, sizeof(label),
-           "MANAGEMENT POLICY = %s     POOL SIZE = %
+  snprintf(label, 80, "%s = %s     %s = %s KB", mp, "mp", ps, "ps");
 }
