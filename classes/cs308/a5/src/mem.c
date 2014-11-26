132346bb6915547b0fd319717c82c248812941aa
rhennigan <rhennigan@gmail.com>
2014-11-26 00:47:20 -0500

    auto-commit changes: Wed Nov 26 00:47:20 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -65,12 +65,12 @@ static inline void print_row(int row) {
       break;
   }
   printf("%s", B_VT);
-  printf("%4s %-6d %s", " ", sn, B_VT);
-  printf("%1s %-6s %s", " ", rq, B_VT);
-  printf("%1s %-7lu %s", " ", sz, B_VT);
-  printf("%1s %-11p %s", " ", ad, B_VT);
+  printf("%1s %-9d %s",   " ", sn, B_VT);
+  printf("%1s %-6s %s",   " ", rq, B_VT);
+  printf("%1s %-7lu %s",  " ", sz, B_VT);
+  printf("%1s %-11p %s",  " ", ad, B_VT);
   printf("%1s %-11lu %s", " ", tf, B_VT);
-  printf(" %-12lu %s", lp, B_VT);
+  printf("%1s %-11lu %s", " ", lp, B_VT);
   printf("\n");
 }
 
