b254c2793ebc1296248f2e612afb08dc333e47f9
rhennigan <rhennigan@gmail.com>
2014-11-26 18:36:51 -0500

    auto-commit changes: Wed Nov 26 18:36:51 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -141,9 +141,9 @@ static mem_block_t * split_block(mem_block_t * block, request_t * request) {
   }
 
   /**************** Free old pointers ******************************************/
-  /* mem_block_t * curr_block = (mem_block_t *)curr_list_node->head; */
-  /* free(curr_block); */
-  /* free(curr_list_node); */
+  mem_block_t * curr_block = (mem_block_t *)curr_list_node->head;
+  free(curr_block);
+  free(curr_list_node);
 
   /*****************************************************************************/
   return alloc_block;
