a53fedee065c0cd3ce191990f3f943a7eff610be
rhennigan <rhennigan@gmail.com>
2014-11-25 19:26:05 -0500

    auto-commit changes: Tue Nov 25 19:26:05 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -12,7 +12,16 @@ int main(int argc, char *argv[]) {
   policy_t policy;
   if (strcmp(argv[1], "first") == 0) {
     policy = FIRST_FIT;
-  };
+  } else if (strcmp(argv[1], "best") == 0) {
+    policy = BEST_FIT;
+  } else if (strcmp(argv[1], "buddy") == 0) {
+    policy = BUDDY_SYSTEM;
+  } else {
+    printf("error reading arguments\n");
+    printf("usage:\n");
+    printf("%s [policy] [pool_size] []\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
   char * policy_str = argv[1];
   size_t pool_size = atoi(argv[2]);
   char * req_file = argv[3];
