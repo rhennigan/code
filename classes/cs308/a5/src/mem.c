17b842065930ad8fde07a63c1c63082fe07f0880
rhennigan <rhennigan@gmail.com>
2014-11-27 03:58:40 -0500

    auto-commit changes: Thu Nov 27 03:58:40 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -286,8 +286,9 @@ int total_granted() {
   return *(int*)list_foldl(history_list, &total, &plus);
 }
 
-static void * get_size(void * x, void * y) {
+static void * get_req_size(void * x, void * y) {
   list_t * list = x;
+  bytes_t 
 }
 
 list_t * size_history() {
