2c8488894f9956718e4d8feffd095335fcfcb4f4
rhennigan <rhennigan@gmail.com>
2014-11-27 00:00:00 -0500

    auto-commit changes: Thu Nov 27 00:00:00 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -94,10 +94,10 @@ mem_block_t * merge_block(mem_block_t * curr_block) {
 
   if (can_merge(next_list)) {
     mem_block_t * next_block = list_head(next_list);
-    curr_block->size += next_block->size;
+    curr_block->size += next_block->size;            // 1
 
     list_t * new_next_list = next_block->next;
-    curr_list->tail = new_next_list;
+    curr_list->tail = new_next_list;                 // 2
   }
 
   return NULL;
