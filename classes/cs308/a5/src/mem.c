2c13880b75987abf170850b429f0718592fbeec1
rhennigan <rhennigan@gmail.com>
2014-11-26 20:50:27 -0500

    auto-commit changes: Wed Nov 26 20:50:27 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -246,7 +246,7 @@ static void print_block(void * block_addr) {
   void *  addr = block.addr;
   bytes_t size = WORDS_TO_BYTES(block.size);
   float   pcnt = (float)size / (float)pool_size;
-  printf("%4d\t%10s\t%10p\t%8lu B\n", blid, free, addr, size);
+  printf("%4d\t%10s\t%10p\t%8lu B\t%5.2f %\n", blid, free, addr, size, pcnt);
 }
 
 void memory_dump() {
