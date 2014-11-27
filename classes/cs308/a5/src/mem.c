b45b04a189679c908ff08133989c406eeb14e9dd
rhennigan <rhennigan@gmail.com>
2014-11-26 20:23:19 -0500

    auto-commit changes: Wed Nov 26 20:23:19 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -196,6 +196,7 @@ static mem_block_t * buddy_free(bytes_t size) {
     mem_block_t * block = list_head(tmp);
     words_t req_words = BYTES_TO_WORDS(size);
     assert(block != NULL);
+
     while (req_words << 1 < block->size) {  // block can be split
       bytes_t   s_bytes   = WORDS_TO_BYTES(block->size >> 1);
       request_t split_req = { NOBODY, ALLOC, s_bytes, 0 };
