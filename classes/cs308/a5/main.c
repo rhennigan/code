04958bd7a6cd79de12063e248e481ff6701504a3
rhennigan <rhennigan@gmail.com>
2014-11-27 01:38:50 -0500

    auto-commit changes: Thu Nov 27 01:38:50 EST 2014

--- classes/cs308/a5/main.c
+++ classes/cs308/a5/main.c
@@ -3,7 +3,7 @@
 #include <limits.h>
 #include "lib/mem.h"
 
-#define STEPS 25
+#define STEPS 150
 
 int main(int argc, char *argv[]) {
   /****************************************************************************/
