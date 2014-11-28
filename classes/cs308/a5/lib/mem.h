28fe5e1bcc75da75b473d539eb773b105b8fb595
rhennigan <rhennigan@gmail.com>
2014-11-28 18:28:43 -0500

    auto-commit changes: Fri Nov 28 18:28:43 EST 2014

--- classes/cs308/a5/lib/mem.h
+++ classes/cs308/a5/lib/mem.h
@@ -94,6 +94,8 @@ request_t *   load_request(FILE * file);
 mem_block_t * allocate_memory(request_t * request);
 mem_block_t * free_memory(request_t * request);
 void          fix_links();
+int64_t       rel_addr(void * a);
+mem_block_t * block_from_list(list_t * list);
 
 /******************************************************************************/
 words_t  total_free();
