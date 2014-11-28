f0f3f73b6369579bb8075a8514c1a1ae210a8b60
rhennigan <rhennigan@gmail.com>
2014-11-28 17:38:42 -0500

    auto-commit changes: Fri Nov 28 17:38:42 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -295,13 +295,12 @@ void fix_links() {
   list_t * prev = NULL;
   list_t * curr = memory_block_list;
   list_t * next = list_tail(curr);
-
-  while (next != NULL) {
+  while (1) {
     mem_block_t * block = ((mem_block_t*)list_head(curr));
     block->prev = prev;
     block->curr = curr;
     block->next = next;
-
+    if (next == NULL) break;
     prev = curr;
     curr = next;
     next = list_tail(next);
