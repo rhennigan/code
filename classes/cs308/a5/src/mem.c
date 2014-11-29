491cd7c68bb26f02939c840be86f9972ddbcaefd
rhennigan <rhennigan@gmail.com>
2014-11-28 21:27:59 -0500

    auto-commit changes: Fri Nov 28 21:27:59 EST 2014

--- classes/cs308/a5/src/mem.c
+++ classes/cs308/a5/src/mem.c
@@ -237,7 +237,7 @@ static mem_block_t * split_block(mem_block_t * block, request_t * request) {
   /* Block size */
   words_t req_words = BYTES_TO_WORDS(request->size);
   words_t req_size  = policy == BUDDY_SYSTEM ?
-                        block->size >> 1 >= req_words ?
+                        (block->size >> 1) >= req_words ?
                           block->size >> 1 :
                           block->size :
                         req_words;
