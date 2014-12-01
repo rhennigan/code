b498b1741662d21d9823b30882c33d9ed7bdce4a
rhennigan <rhennigan@gmail.com>
2014-11-30 20:41:24 -0500

    auto-commit changes: Sun Nov 30 20:41:24 EST 2014

--- c/lib/list.h
+++ c/lib/list.h
@@ -14,23 +14,33 @@ typedef struct list_s {
   struct list_s * tail;
 } list_t;
 
+typedef struct lpair_s {
+  list_t * left;
+  list_t * right;
+} lpair_t;
+
 typedef bool (*cmp_fun)(void * a, void * b);
 
 list_t * list_app(list_t * list, void * data);
 list_t * list_copy(list_t * list);
 void     list_dispose(list_t * list);
 void     list_dump(list_t * list);
-void *   list_find(list_t * list, void * h, cmp_fun eq);
+list_t * list_extremum(list_t * list, cmp_fun ex);
+list_t * list_filter(list_t * list, cmp_fun pred, void * cmp_arg);
+list_t * list_find(list_t * list, void * match, cmp_fun eq);
+void *   list_foldl(list_t * list, void * acc, void * (*f)(void * x, void * y));
+void *   list_foldr(list_t * list, void * acc, void * (*f)(void * x, void * y));
 list_t * list_fromarray(void * array, size_t objsize, size_t length);
-void *   list_head(list_t * list);
-void     list_iter(list_t * list, void (*f)(void * head));
+void   * list_head(list_t * list);
+void     list_iter(list_t * list, void (*f)(void * data));
 list_t * list_join(list_t * list1, list_t * list2);
 size_t   list_length(list_t * list);
 list_t * list_map(list_t * list, void * (*f)(void * x));
+lpair_t  list_partition(list_t * lst, cmp_fun pred, void * cmp_arg);
 list_t * list_pre(list_t * list, void * data);
 list_t * list_reverse(list_t * list);
 list_t * list_sort(list_t * list, cmp_fun lt);
 list_t * list_tail(list_t * list);
-void *   list_toarray(list_t * list, size_t size);
+void   * list_toarray(list_t * list, size_t size);
 
 #endif  // LIB_LIST_H_
--- c/src/list.c
+++ c/src/list.c
@@ -41,6 +41,27 @@ static inline list_t * list_snoc(list_t * list, void * data) {
   }  // end if (list == NULL)
 }  // end list_snoc
 
+static inline list_t * merge(list_t * xxs, list_t * yys, cmp_fun lt) {
+  if (xxs == NULL && yys == NULL) {
+    return NULL;
+  } else if (xxs == NULL) {
+    return yys;
+  } else if (yys == NULL) {
+    return xxs;
+  } else {  // (xs != NULL && ys != NULL)
+    void * x = list_head(xxs);
+    void * y = list_head(yys);
+    list_t * xs = list_tail(xxs);
+    list_t * ys = list_tail(yys);
+    list_t * merged = merge(xs, ys, lt);
+    if (lt(x, y)) {
+      return list_cons(list_cons(merged, y), x);
+    } else {  // (!lt(x, y))
+      return list_cons(list_cons(merged, x), y);
+    }  // end if (lt(x, y))
+  }  // end if (xs == NULL && ys == NULL)
+}  // end merge
+
 /******************************************************************************/
 /* PUBLIC FUNCTIONS                                                           */
 /******************************************************************************/
@@ -63,10 +84,9 @@ list_t * list_copy(list_t * list) {
 
 void list_dispose(list_t * list) {
   if (list == NULL) return;
-  list_t ** next = &list->tail;
+  list_t * tail = list->tail;
   free(list);
-  list = NULL;
-  list_dispose(*next);
+  list_dispose(tail);
 }  // end list_dispose
 
 void list_dump(list_t * list) {
@@ -85,14 +105,55 @@ void list_dump(list_t * list) {
   }  // end if (list == NULL)
 }  // end list_dump
 
-void * list_find(list_t * list, void * h, cmp_fun eq) {
+list_t * list_extremum(list_t * list, cmp_fun ex) {
   if (list == NULL) {
     return NULL;
-  } else if ((*eq)(h, list_head(list))) {
-    return list_head(list);
+  } else {  // (list != NULL)
+    list_t * keep = list;
+    list_t * next = list;
+    while ((next = list_tail(next)) != NULL) {
+      if (ex(list_head(next), list_head(keep))) {
+        keep = next;
+      }  // end if (ex(list_head(next), list_head(keep)))
+    }  // end while (list_tail(next) != NULL)
+    return keep;
+  }  // end if (list == NULL)
+}  // end list_extremum
+
+list_t * list_filter(list_t * list, cmp_fun pred, void * cmp_arg) {
+  lpair_t pair = list_partition(list, pred, cmp_arg);
+  list_dispose(pair.right);
+  return pair.left;
+}  // end list_filter
+
+void * list_foldl(list_t * list, void * acc, void * (*f)(void * x, void * y)) {
+  if (list == NULL) {
+    return acc;
+  } else {
+    void   * x  = list_head(list);
+    list_t * xs = list_tail(list);
+    return list_foldl(xs, f(acc, x), f);
+  }
+}
+
+void * list_foldr(list_t * list, void * acc, void * (*f)(void * x, void * y)) {
+  if (list == NULL) {
+    return acc;
+  } else {
+    void   * x  = list_head(list);
+    list_t * xs = list_tail(list);
+    return f(x, list_foldr(xs, acc, f));  // aka "the stack smasher"
+  }
+}
+
+list_t * list_find(list_t * list, void * match, cmp_fun eq) {
+  if (list == NULL) {
+    return NULL;
+  } else if ((*eq)(list, match)) {
+    return list;
   } else {  // (list != NULL && !(*cmp)(h, list_head(list)))
-    return list_find(list_tail(list), h, (*eq));
-  }   // end if (list == NULL)
+    return list_find(list_tail(list), match, (*eq));
+  }  // end if (list == NULL)
 }  // end list_find
 
 list_t * list_fromarray(void * array, size_t objsize, size_t length) {
@@ -122,8 +183,12 @@ void list_iter(list_t * list, void (*f)(void * head)) {
 
 list_t * list_join(list_t * list1, list_t * list2) {
   list_t * list = list_copy(list1);
-  last_node(list)->tail = list2;
-  return list;
+  if (list == NULL) {
+    return list2;
+  } else {  // (list != NULL)
+    last_node(list)->tail = list2;
+    return list;
+  }  // end if (list == NULL)
 }  // end list_join
 
 size_t list_length(list_t * list) {
@@ -143,6 +208,20 @@ list_t * list_map(list_t * list, void * (*f)(void * x)) {
   }  // end if (list == NULL)
 }
 
+lpair_t list_partition(list_t * list, cmp_fun pred, void * pivot) {
+  lpair_t pair = { NULL, NULL };
+  while (list != NULL) {
+    void * x = list_head(list);
+    if (pred(x, pivot)) {
+      pair.left = list_cons(pair.left, x);
+    } else {  // (!pred(x, pivot))
+      pair.right = list_cons(pair.right, x);
+    }  // end if (pred(x, pivot))
+    list = list_tail(list);
+  }  // end while (list != NULL)
+  return pair;
+}
+
 list_t * list_pre(list_t * list, void * data) {
   return list_cons(list, data);
 }  // end list_app
@@ -155,6 +234,23 @@ list_t * list_reverse(list_t * list) {
   }  // end while (list != NULL)
   return new_list;
 }  // end list_reverse
+
+list_t * list_sort(list_t * list, cmp_fun lt) {
+  printf("\n\nsorting list: \n");
+  list_dump(list);
+  fflush(NULL);
+  if (list == NULL) {
+    return NULL;
+  } else if (list_tail(list) == NULL) {
+    return list;
+  } else {  // (list != NULL)
+    void * pivot = list_head(list);
+    lpair_t part = list_partition(list_tail(list), lt, pivot);
+    part.left = list_sort(part.left, lt);
+    part.right = list_cons(list_sort(part.right, lt), pivot);
+    return list_join(part.left, part.right);
+  }  // end if (list == NULL)
+}  // end list_sort
 
 inline list_t * list_tail(list_t * list) {
   if (list == NULL) {
