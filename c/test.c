143c391449c2ef745c2232d33819525dc695b8ee
rhennigan <rhennigan@gmail.com>
2014-11-30 23:37:21 -0500

    auto-commit changes: Sun Nov 30 23:37:21 EST 2014

--- c/test.c
+++ c/test.c
@@ -20,21 +20,19 @@ int main(int argc, char *argv[]) {
   TYPE * array = malloc(sizeof(TYPE) * COUNT);
   assert(array != NULL);
 
-clock_t start = clock(), diff;
-ProcessIntenseFunction();
-diff = clock() - start;
-
-int msec = diff * 1000 / CLOCKS_PER_SEC;
-printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
-  int start = time(NULL);
+  clock_t start = clock(), diff;
   for (TYPE i = 0; i < COUNT; i++) {
     array[i] = i;
   }
-  printf("fill array time = %d\n", (int)time(NULL) - start);
+  diff = clock() - start;
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("fill array time = %d seconds %d milliseconds\n", msec/1000, msec%1000);
 
-  start = time(NULL);
+  start = clock();
   list_t * list = list_fromarray(array, sizeof(TYPE), COUNT);
-  printf("fill list time = %d\n", (int)time(NULL) - start);
+  diff = clock() - start;
+  msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("fill list time = %d seconds %d milliseconds\n", msec/1000, msec%1000);
 
   TYPE total1 = 0;
   TYPE total2 = 0;
