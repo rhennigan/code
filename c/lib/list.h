f63ad498223793a5210b0915d1217272da89b027
rhennigan <rhennigan@gmail.com>
2014-11-30 21:26:24 -0500

    auto-commit changes: Sun Nov 30 21:26:24 EST 2014

--- c/lib/list.h
+++ c/lib/list.h
@@ -26,6 +26,7 @@ typedef bool   (*dyn_cmp_f)(data_t head1, data_t head2, void * dep_arg);
 typedef bool   (*sta_pred_f)(data_t head);
 typedef bool   (*dyn_pred_f)(data_t head, void * dep_arg);
 typedef void * (*fold_f)(void * acc, data_t head);
+typedef data_t (*map_f)(data_t data);
 
 void     list_app(list_t * list, void * data);
 list_t * list_copy(list_t * list);
