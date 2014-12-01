905661e12732b875d4ef21730c74a55d608aaf67
rhennigan <rhennigan@gmail.com>
2014-12-01 03:33:29 -0500

    auto-commit changes: Mon Dec  1 03:33:29 EST 2014

--- c/timing.sh
+++ c/timing.sh
@@ -2,7 +2,7 @@
 
 for j in `seq 1 1000000`;
 do
-	for i in `seq 1000000 1000000 10000000`;
+	for i in `seq 1000000 1000000 100000000`;
 	do
 		output=$(./test $i | grep 'sort time' | sed 's/sort//g' | sed 's/time//g' | sed 's/= //g')
 		echo $i, $output >> data$1.csv
