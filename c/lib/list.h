d90e0a6757f124e63408b24df9aa0306b95604d8
rhennigan <rhennigan@gmail.com>
2014-11-30 20:50:38 -0500

    auto-commit changes: Sun Nov 30 20:50:38 EST 2014

--- c/lib/list.h
+++ c/lib/list.h
@@ -29,7 +29,7 @@ void     list_dispose(list_t * list);
 void     list_dump(list_t * list);
 list_t * list_extremum(list_t * list, cmp_f ex);
 list_t * list_filter(list_t * list, dyn_pred_f pred, void * dep_args);
-list_t * list_find(list_t * list, void * match, cmp_fun eq);
+list_t * list_find(list_t * list, dyn_pred_f pred, void * dep_args);
 void *   list_foldl(list_t * list, void * acc, void * (*f)(void * x, void * y));
 void *   list_foldr(list_t * list, void * acc, void * (*f)(void * x, void * y));
 list_t * list_fromarray(void * array, size_t objsize, size_t length);
