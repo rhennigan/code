af3058d7e4b95eb5e4b1a939b953796200c82959
rhennigan <rhennigan@gmail.com>
2014-11-30 22:26:55 -0500

    auto-commit changes: Sun Nov 30 22:26:55 EST 2014

--- c/src/list.c
+++ c/src/list.c
@@ -270,8 +270,10 @@ list_t * list_reverse(list_t * list) {
 }  // end list_reverse
 
 list_t * list_reverse2(list_t * list) {
+  list_t * temp = list;
   list_t * tail = list;
   while (tail != NULL) {
+    tail = list_tail(tail);
     
   }
 }
