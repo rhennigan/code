b76578d5167d0ed6724990622fcb690701f76721
rhennigan <rhennigan@gmail.com>
2014-11-30 21:49:04 -0500

    auto-commit changes: Sun Nov 30 21:49:04 EST 2014

--- c/src/list.c
+++ c/src/list.c
@@ -160,13 +160,20 @@ void * list_foldr(list_t * list, void * acc, fold_f f) {
 }
 
 list_t * list_find(list_t * list, dyn_pred_f pred, void * dep_arg) {
-  if (list == NULL) {
-    return NULL;
-  } else if (pred(list, dep_arg)) {
-    return list;
-  } else {  // (list != NULL && !(*cmp)(h, list_head(list)))
-    return list_find(list_tail(list), pred, dep_arg);
-  }  // end if (list == NULL)
+  while (list != NULL) {
+    if (pred(list_head(list), dep_arg)) {
+      return list;
+    } else {
+      list = list_tail(list);
+    }
+  }
+  /* if (list == NULL) { */
+  /*   return NULL; */
+  /* } else if (pred(list, dep_arg)) { */
+  /*   return list; */
+  /* } else {  // (list != NULL && !(*cmp)(h, list_head(list))) */
+  /*   return list_find(list_tail(list), pred, dep_arg); */
+  /* }  // end if (list == NULL) */
 }  // end list_find
 
 list_t * list_fromarray(void * array, size_t objsize, size_t length) {
