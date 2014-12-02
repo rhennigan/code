10a44bdff8b4faa59ff7a0b930063d8fa5d539d6
rhennigan <rhennigan@gmail.com>
2014-12-02 08:18:56 -0500

    auto-commit changes: Tue Dec  2 08:18:56 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -164,6 +164,7 @@ void display_fs_node(void * node_addr) {
     b = f_info->d_name[i] == '/' ? i : b;
 
   printf("%s%s %s %s\n", os, lbl_color, f_info->d_name + b, C_OFF);
+  printf("%s  %s\n",  os, mstr);
   /* printf("%s d_off       = %ld\n", os, f_info->d_off); */
   /* printf("%s d_reclen    = %u\n",  os, f_info->d_reclen); */
   /* printf("%s d_type      = %s\n",  os, type_names[type]); */
@@ -171,7 +172,7 @@ void display_fs_node(void * node_addr) {
   /* printf("%s dev_min     = %u\n",  os, f_info->dev_min); */
   /* printf("%s st_ino      = %ld\n", os, f_info->st_ino); */
   /* printf("%s st_mode     = %u\n",  os, f_info->st_mode); */
-  printf("%s   mstr        = %s\n",  os, mstr);
+  /* printf("%s   mstr        = %s\n",  os, mstr); */
   /* printf("%s st_uid      = %d\n",  os, f_info->st_uid); */
   printf("%s   user_name   = %s\n",  os, user_name);
   /* printf("%s user_pw     = %s\n",  os, user_pw); */
