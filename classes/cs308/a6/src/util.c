84a616c3c532628a17d82f188e0900e99e364335
rhennigan <rhennigan@gmail.com>
2014-12-02 10:11:33 -0500

    auto-commit changes: Tue Dec  2 10:11:33 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -203,8 +203,10 @@ void display_fs_node(void * node_addr) {
 
   bool exec_b = (mode & S_IXUSR) || (mode & S_IXGRP) || (mode & S_IXOTH);
   const char * lbl_color = (type == DT_REG && exec_b) ?
-                           C_BIGreen :
-                           type_colors[type];
+                             C_BIGreen :
+                             ndir(f_info) ?
+                               C_IWhite :
+                               type_colors[type];
   size_t b = 2;
   for (size_t i = b; i < strlen(f_info->d_name); i++)
     b = f_info->d_name[i] == '/' ? i : b;
