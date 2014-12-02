e17bc5cd9481a269ecd93eec6247d49873262e37
rhennigan <rhennigan@gmail.com>
2014-12-02 05:53:50 -0500

    auto-commit changes: Tue Dec  2 05:53:50 EST 2014

--- classes/cs308/a6/main.c
+++ classes/cs308/a6/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   list_t * unsorted_entries = dir_list(dir_name, 2);
   list_t * entries = list_sort(unsorted_entries, &name_cmp);
   list_iter(entries, &display_fs_node);
+  list_iter(entries, &free);
   list_dispose(entries);
   return 0;
 }
