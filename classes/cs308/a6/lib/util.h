e1ecdf1a45548b29cdb464c22de4c9b3f61a3587
rhennigan <rhennigan@gmail.com>
2014-12-02 03:34:49 -0500

    auto-commit changes: Tue Dec  2 03:34:49 EST 2014

--- classes/cs308/a6/lib/util.h
+++ classes/cs308/a6/lib/util.h
@@ -16,6 +16,24 @@ typedef struct fsys_node_s {
   unsigned char  d_type;
   char           d_name[NAME_MAX];
   size_t         depth;
+  dev_t          st_dev;         /* ID of device containing file */
+  ino_t          st_ino;         /* inode number */
+  mode_t         st_mode;        /* protection */
+  nlink_t        st_nlink;       /* number of hard links */
+  uid_t          st_uid;         /* user ID of owner */
+  gid_t          st_gid;         /* group ID of owner */
+  dev_t          st_rdev;        /* device ID (if special file) */
+  off_t          st_size;        /* total size, in bytes */
+  blksize_t      st_blksize;     /* blocksize for filesystem I/O */
+  blkcnt_t       st_blocks;      /* number of 512B blocks allocated */
+
+  /* Since Linux 2.6, the kernel supports nanosecond
+     precision for the following timestamp fields.
+     For the details before Linux 2.6, see NOTES. */
+
+  struct timespec st_atim;  /* time of last access */
+  struct timespec st_mtim;  /* time of last modification */
+  struct timespec st_ctim;  /* time of last status change */
 } fsys_node_t;
 
 list_t * dir_list(char * dir_name, size_t depth);
