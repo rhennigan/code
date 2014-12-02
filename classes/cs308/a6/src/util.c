bde63dddb6b6a80f3c3a33b637d2fd1845a94c91
rhennigan <rhennigan@gmail.com>
2014-12-02 09:27:19 -0500

    auto-commit changes: Tue Dec  2 09:27:19 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -197,7 +197,7 @@ void display_fs_node(void * node_addr) {
   printf(" %s",        gr_name);
   printf(" %lu",       f_info->st_nlink);
   printf(" %s",        byte_str(f_info->st_size));
-  printf(" %ld\n",     f_info->d_ino);
+  printf(" %ld",     f_info->d_ino);
   
   /* printf("%s d_off       = %ld\n", os, f_info->d_off); */
   /* printf("%s d_reclen    = %u\n",  os, f_info->d_reclen); */
