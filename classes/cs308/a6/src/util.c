e1c2ccc5f2c9f7cfc057c78261f6edf921aa4a63
rhennigan <rhennigan@gmail.com>
2014-12-02 05:03:51 -0500

    auto-commit changes: Tue Dec  2 05:03:51 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -122,6 +122,9 @@ void display_fs_node(void * node_addr) {
 
   char * user_name = pw->pw_name;
   char * user_pw   = pw->pw_passwd;
+  char * user_info = pw->pw_gecos;
+  char * user_home = pw->pw_dir;
+  char * user_shll = pw->pw_shell;
 
   printf("%s%s %s %s\n",        os, type_colors[type], f_info->d_name, C_OFF);
   printf("%s d_ino     = %ld\n", os, f_info->d_ino);
