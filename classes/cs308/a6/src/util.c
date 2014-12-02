43c40a968b452901aa97b76a1eaebad597ce089e
rhennigan <rhennigan@gmail.com>
2014-12-02 05:47:42 -0500

    auto-commit changes: Tue Dec  2 05:47:42 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -162,9 +162,9 @@ void display_fs_node(void * node_addr) {
   printf("%s st_size     = %lu\n", os, f_info->st_size);
   printf("%s st_blksize  = %lu\n", os, f_info->st_blksize);
   printf("%s st_blocks   = %lu\n", os, f_info->st_blocks);
-  printf("%s atime       = %s",  os, ctime(&f_info->atime));
-  printf("%s mtime       = %s",  os, ctime(&f_info->mtime));
-  printf("%s ctime       = %s",  os, ctime(&f_info->ctime));
+  printf("%s atime       = %s",    os, ctime(&f_info->atime));
+  printf("%s mtime       = %s",    os, ctime(&f_info->mtime));
+  printf("%s ctime       = %s",    os, ctime(&f_info->ctime));
   printf("\n");
 }
 
