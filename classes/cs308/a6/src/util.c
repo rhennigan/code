7cdc18b5be4e5d0c3ca2bab64e59cf410cfedac7
rhennigan <rhennigan@gmail.com>
2014-12-02 04:29:02 -0500

    auto-commit changes: Tue Dec  2 04:29:02 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -77,10 +77,10 @@ list_t * dir_list(char * dir_name, size_t depth) {
 }
 
 /****************************************************************************/
-static inline const char * byte_to_binary(int x) {
+static inline const char * byte_to_binary(unsigned int x) {
     static char b[9];
     b[0] = '\0';
-    for (int z = 128; z > 0; z >>= 1)
+    for (unsigned int z = 128; z > 0; z >>= 1)
         strcat(b, ((x & z) == z) ? "1" : "0");
     return b;
 }
