53ef4ab66625b1cf6a3760dd47bbe2f7415cf540
rhennigan <rhennigan@gmail.com>
2014-12-02 10:07:27 -0500

    auto-commit changes: Tue Dec  2 10:07:27 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -257,7 +257,10 @@ void display_fs_node(void * node_addr) {
   if (f_info->sub_nodes != NULL) {
     list_iter(f_info->sub_nodes, &display_fs_node);
     pv(f_info->depth);
-    printf(" %s\n", C_BIWhite B_BL B_HR C_OFF);
+    printf(" %s", C_BIWhite B_BL);
+    for (int i = 0; i < 10; i++)
+      printf("%s", B_HR);
+    printf("%s\n", C_OFF);
   }
 }
 
