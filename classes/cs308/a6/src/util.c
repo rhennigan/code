d00e9682c43a368acec2c0ffec231e257063f0a4
rhennigan <rhennigan@gmail.com>
2014-12-03 05:43:34 -0500

    auto-commit changes: Wed Dec  3 05:43:34 EST 2014

--- classes/cs308/a6/src/util.c
+++ classes/cs308/a6/src/util.c
@@ -330,7 +330,7 @@ void create_rand(char * rand_path, size_t size) {
   char buffer[BUFSIZ];
   int  urandom = open("/dev/urandom", O_RDONLY);
   int  randomf = open(rand_path, O_WRONLY);
-  size_t bytes_read, bytes_sent, total = 0;
+  size_t total = 0;
 
   if (urandom == -1) {
     perror("open urandom");
@@ -343,14 +343,14 @@ void create_rand(char * rand_path, size_t size) {
   }
 
   while (total < size) {
-    bytes_read =  read(urandom, buffer, BUFSIZ);
+    size_t bytes_read = read(urandom, buffer, BUFSIZ);
 
     if (bytes_read < BUFSIZ) {
       perror("read urandom");
       exit(EXIT_FAILURE);
     }
 
-    bytes_sent = write(randomf, buffer, BUFSIZ);
+    size_t bytes_sent = write(randomf, buffer, BUFSIZ);
 
     if (bytes_sent != bytes_read) {
       perror("write randomf");
