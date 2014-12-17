173a7c7f5409f36c5202a469bba7bbcebfe52e0b
rhennigan <rhennigan@gmail.com>
2014-12-17 13:12:34 -0500

    auto-commit changes: Wed Dec 17 13:12:34 EST 2014

---
+++ c/lib/pretty_term_printing.h
@@ -0,0 +1,85 @@
+/* Reset */
+#define C_OFF           "\e[0m"       // Text Reset
+
+/* Regular Colors */
+#define C_BLACK         "\e[0;30m"    // Black
+#define C_RED           "\e[0;31m"    // Red
+#define C_GREEN         "\e[0;32m"    // Green
+#define C_YELLOW        "\e[0;33m"    // Yellow
+#define C_BLUE          "\e[0;34m"    // Blue
+#define C_PURPLE        "\e[0;35m"    // Purple
+#define C_CYAN          "\e[0;36m"    // Cyan
+#define C_WHITE         "\e[0;37m"    // White
+
+/* Bold */
+#define C_BBLACK        "\e[1;30m"    // Black
+#define C_BRED          "\e[1;31m"    // Red
+#define C_BGREEN        "\e[1;32m"    // Green
+#define C_BYELLOW       "\e[1;33m"    // Yellow
+#define C_BBLUE         "\e[1;34m"    // Blue
+#define C_BPURPLE       "\e[1;35m"    // Purple
+#define C_BCYAN         "\e[1;36m"    // Cyan
+#define C_BWHITE        "\e[1;37m"    // White
+
+/* Underline */
+#define C_UBlack        "\e[4;30m"    // Black
+#define C_URed          "\e[4;31m"    // Red
+#define C_UGreen        "\e[4;32m"    // Green
+#define C_UYellow       "\e[4;33m"    // Yellow
+#define C_UBlue         "\e[4;34m"    // Blue
+#define C_UPurple       "\e[4;35m"    // Purple
+#define C_UCyan         "\e[4;36m"    // Cyan
+#define C_UWhite        "\e[4;37m"    // White
+
+/* Background */
+#define C_On_Black      "\e[40m"      // Black
+#define C_On_Red        "\e[41m"      // Red
+#define C_On_Green      "\e[42m"      // Green
+#define C_On_Yellow     "\e[43m"      // Yellow
+#define C_On_Blue       "\e[44m"      // Blue
+#define C_On_Purple     "\e[45m"      // Purple
+#define C_On_Cyan       "\e[46m"      // Cyan
+#define C_On_White      "\e[47m"      // White
+
+/* High Intensity */
+#define C_IBlack        "\e[0;90m"    // Black
+#define C_IRed          "\e[0;91m"    // Red
+#define C_IGreen        "\e[0;92m"    // Green
+#define C_IYellow       "\e[0;93m"    // Yellow
+#define C_IBlue         "\e[0;94m"    // Blue
+#define C_IPurple       "\e[0;95m"    // Purple
+#define C_ICyan         "\e[0;96m"    // Cyan
+#define C_IWhite        "\e[0;97m"    // White
+
+/* Bold High Intensity */
+#define C_BIBlack       "\e[1;90m"    // Black
+#define C_BIRed         "\e[1;91m"    // Red
+#define C_BIGreen       "\e[1;92m"    // Green
+#define C_BIYellow      "\e[1;93m"    // Yellow
+#define C_BIBlue        "\e[1;94m"    // Blue
+#define C_BIPurple      "\e[1;95m"    // Purple
+#define C_BICyan        "\e[1;96m"    // Cyan
+#define C_BIWhite       "\e[1;97m"    // White
+
+/* High Intensity backgrounds */
+#define C_On_IBlack     "\e[0;100m"   // Black
+#define C_On_IRed       "\e[0;101m"   // Red
+#define C_On_IGreen     "\e[0;102m"   // Green
+#define C_On_IYellow    "\e[0;103m"   // Yellow
+#define C_On_IBlue      "\e[0;104m"   // Blue
+#define C_On_IPurple    "\e[0;105m"   // Purple
+#define C_On_ICyan      "\e[0;106m"   // Cyan
+#define C_On_IWhite     "\e[0;107m"   // White
+
+/* Unicode block characters */
+#define B_HR "\u2500"  // horizontal bar
+#define B_TL "\u250C"  // top-left box corner
+#define B_TR "\u2510"  // top-right box corner
+#define B_BL "\u2514"  // bottom-left box corner
+#define B_BR "\u2518"  // bottom-right box corner
+#define B_VT "\u2502"  // vertical bar
+#define B_LM "\u251C"  // left-middle join
+#define B_RM "\u2524"  // right-middle join
+#define B_TM "\u252C"  // top-middle join
+#define B_BM "\u2534"  // bottom-middle join
+#define B_CM "\u253C"  // center-middle join
