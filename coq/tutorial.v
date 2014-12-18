478fa23675c570faa1b21b6740865242acf60561
rhennigan <rhennigan@gmail.com>
2014-12-18 17:10:26 -0500

    auto-commit changes: Thu Dec 18 17:10:26 EST 2014

--- coq/tutorial.v
+++ coq/tutorial.v
@@ -86,10 +86,7 @@ Proof.
   intros A_Prop B_Prop.
   intro Not_A_im_B.
   unfold not in Not_A_im_B.
-  
-  apply (contrapositive A_Prop B_Prop) in Not_A_im_B.
-  
-  apply contrapositive in suppose_not_A_im_B.
+  apply (contrapositive (A_Prop -> B_Prop)) in Not_A_im_B.
 
 Lemma Peirce_neg_alt : ~ (((A -> B) -> A) -> A) -> False.
 Proof.
