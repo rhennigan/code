6cd26c733d3a8322fc35db918c9d3b47243487b4
rhennigan <rhennigan@gmail.com>
2014-12-18 17:11:28 -0500

    auto-commit changes: Thu Dec 18 17:11:28 EST 2014

--- coq/tutorial.v
+++ coq/tutorial.v
@@ -85,7 +85,7 @@ Lemma neg_implication :
 Proof.
   intros A_Prop B_Prop.
   intro Not_A_im_B.
-  unfold not in Not_A_im_B.
+  (* unfold not in Not_A_im_B. *)
   apply (contrapositive (A_Prop -> B_Prop)) in Not_A_im_B.
 
 Lemma Peirce_neg_alt : ~ (((A -> B) -> A) -> A) -> False.
