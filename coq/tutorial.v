d38b785b5e9c7aefc1908627bb201834ca51a525
rhennigan <rhennigan@gmail.com>
2014-12-18 18:33:28 -0500

    auto-commit changes: Thu Dec 18 18:33:28 EST 2014

--- coq/tutorial.v
+++ coq/tutorial.v
@@ -116,25 +116,27 @@ Section Predicate_calculus.
 
   Variable R : D -> D -> Prop.
 
-  Hypothesis R_symmetric : 
-    forall x y : D, R x y -> R y x.
+  Section R_sym_trans.
+    Hypothesis R_symmetric : 
+      forall x y : D, R x y -> R y x.
 
-  Hypothesis R_transitive : 
-    forall x y z : D, R x y /\ R y z -> R x z.
+    Hypothesis R_transitive : 
+      forall x y z : D, R x y /\ R y z -> R x z.
 
-  Lemma refl_if : forall x : D, (exists y, R x y) -> R x x.
-  Proof.
-    intros x x_Rlinked.
-    elim x_Rlinked.
-    intro y.
-    intro Rxy.
-    apply R_symmetric in Rxy.
-    apply R_transitive with y.
-    split.
-    apply R_symmetric.
-    exact Rxy.
-    exact Rxy.
-  Qed.
+    Lemma refl_if : forall x : D, (exists y, R x y) -> R x x.
+    Proof.
+      intros x x_Rlinked.
+      elim x_Rlinked.
+      intro y.
+      intro Rxy.
+      apply R_symmetric in Rxy.
+      apply R_transitive with y.
+      split.
+      apply R_symmetric.
+      exact Rxy.
+      exact Rxy.
+    Qed.
 
-  Print refl_if.
-  
+    Print refl_if.
+    
+  End R_sym_trans.
