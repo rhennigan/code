b82428dd5a37127bbb11d2ecbed4b8cedffc8947
rhennigan <rhennigan@gmail.com>
2014-12-18 18:54:09 -0500

    auto-commit changes: Thu Dec 18 18:54:09 EST 2014

--- coq/tutorial.v
+++ coq/tutorial.v
@@ -148,6 +148,6 @@ Section Predicate_calculus.
 
     Hypothesis LEM : forall A : Prop, A \/ ~ A.
     Lemma smullyan : exists x : D, P x -> forall x : D, P x.
-      exists x.
+      elim (LEM (exists x : D, ~ P x)).
       
   End Predicate_paradox.
