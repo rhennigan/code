a7cfac2e08681ef3b757863f88cb94da816b43e6
rhennigan <rhennigan@gmail.com>
2014-12-20 03:55:46 -0500

    auto-commit changes: Sat Dec 20 03:55:46 EST 2014

--- coq/sf_basic.v
+++ coq/sf_basic.v
@@ -393,3 +393,12 @@ Proof.
   reflexivity.
 Qed.
 
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool), (forall (x : bool), f x = negb x) -> forall (b : bool), f (f b) = b.
+Proof.
+  intro f.
+  intro H_fx_neq_x.
+  intro b.
+  rewrite -> H_fx_neq_x.
+  rewrite -> H_fx_neq_x.
+  destruct.
