fbc55a78b019f172072f233d5740e32858bf8e1c
rhennigan <rhennigan@gmail.com>
2014-12-20 02:30:24 -0500

    auto-commit changes: Sat Dec 20 02:30:24 EST 2014

--- coq/sf_basic.v
+++ coq/sf_basic.v
@@ -50,54 +50,56 @@ Section Basics.
       | _, _ => false
     end.
 
-Example test_nandb1: (nandb true false) = true.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_nandb1: (nandb true false) = true.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_nandb2: (nandb false false) = true.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_nandb2: (nandb false false) = true.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_nandb3: (nandb false true) = true.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_nandb3: (nandb false true) = true.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_nandb4: (nandb true true) = false.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_nandb4: (nandb true true) = false.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Definition andb3 (a b c : bool) : bool := andb a (andb b c).
+  Definition andb3 (a b c : bool) : bool := andb a (andb b c).
 
-Example test_andb31: (andb3 true true true) = true.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_andb31: (andb3 true true true) = true.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_andb32: (andb3 false true true) = false.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_andb32: (andb3 false true true) = false.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_andb33: (andb3 true false true) = false.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_andb33: (andb3 true false true) = false.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Example test_andb34: (andb3 true true false) = false.
-Proof.
-  simpl.
-  reflexivity.
-Qed.
+  Example test_andb34: (andb3 true true false) = false.
+  Proof.
+    simpl.
+    reflexivity.
+  Qed.
 
-Check negb.
+  Check negb.
+
+  Module NatTest.
