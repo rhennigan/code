b5a99d05330877750edd853ab7ef1f1ebbda9428
rhennigan <rhennigan@gmail.com>
2014-12-20 03:01:08 -0500

    auto-commit changes: Sat Dec 20 03:01:08 EST 2014

--- coq/sf_basic.v
+++ coq/sf_basic.v
@@ -206,3 +206,10 @@ Proof.
   simpl.
   reflexivity.
 Qed.
+
+Notation "x + y" := (plus x y) (at level 50, left associativity) : nat_scope.
+Notation "x - y" := (minus x y) (at level 50, left associativity) : nat_scope.
+Notation "x * y" := (mult x y) (at level 40, left associativity) : nat_scope.
+
+Check ((0 + 1) + 1).
+Eval compute in ((0 + 1) + 1).
