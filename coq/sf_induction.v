430320d3ffab061dbafda7f805f87cc2e2736736
rhennigan <rhennigan@gmail.com>
2014-12-20 06:29:25 -0500

    auto-commit changes: Sat Dec 20 06:29:25 EST 2014

---
+++ coq/sf_induction.v
@@ -0,0 +1,73 @@
+Require Export sf_basic.
+
+Require String. Open Scope string_scope.
+
+Ltac move_to_top x :=
+  match reverse goal with
+  | H : _ |- _ => try move x after H
+  end.
+
+Tactic Notation "assert_eq" ident(x) constr(v) :=
+  let H := fresh in
+  assert (x = v) as H by reflexivity;
+  clear H.
+
+Tactic Notation "Case_aux" ident(x) constr(name) :=
+  first [
+    set (x := name); move_to_top x
+  | assert_eq x name; move_to_top x
+  | fail 1 "because we are working on a different case" ].
+
+Tactic Notation "Case" constr(name) := Case_aux Case name.
+Tactic Notation "SCase" constr(name) := Case_aux SCase name.
+Tactic Notation "SSCase" constr(name) := Case_aux SSCase name.
+Tactic Notation "SSSCase" constr(name) := Case_aux SSSCase name.
+Tactic Notation "SSSSCase" constr(name) := Case_aux SSSSCase name.
+Tactic Notation "SSSSSCase" constr(name) := Case_aux SSSSSCase name.
+Tactic Notation "SSSSSSCase" constr(name) := Case_aux SSSSSSCase name.
+Tactic Notation "SSSSSSSCase" constr(name) := Case_aux SSSSSSSCase name.
+
+Theorem andb_true_elim1 : forall a b : bool, andb a b = true -> a = true.
+Proof.
+  intros a b.
+  intro H.
+  destruct a.
+  Case "a = true". 
+  {
+    simpl.
+    reflexivity. 
+  }
+  Case "a = false".
+  {
+    rewrite <- H.
+    simpl.
+    reflexivity.
+  }
+Qed.
+
+Theorem andb_true_elim2 : forall a b : bool, andb a b = true -> b = true.
+Proof.
+  intros a b.
+  intro H.
+  destruct b.
+  Case "b = true".
+  {
+    simpl.
+    reflexivity.
+  }
+  Case "b = false".
+  {
+    rewrite <- H.
+    destruct a.
+    SCase "a = true".
+    {
+      simpl.
+      reflexivity.
+    }
+    SCase "a = false".
+    {
+      simpl.
+      reflexivity.
+    }
+  }
+Qed.
