25e86982c6cfeef7c19db517bd5e3e123d7e66fc
rhennigan <rhennigan@gmail.com>
2014-12-20 02:15:03 -0500

    auto-commit changes: Sat Dec 20 02:15:03 EST 2014

---
+++ coq/sf_basic.v
@@ -0,0 +1,21 @@
+Section Basics.
+  Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+  Definition next_weekday (d : day) : day :=
+    match d with
+      | monday => tuesday
+      | tuesday => wednesday
+      | wednesday => thursday
+      | thursday => friday
+      | _ => monday
+    end.
+
+Eval compute in (next_weekday friday).
+Eval compute in (next_weekday (next_weekday saturday)).
