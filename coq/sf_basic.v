7f35cc1c4a98bb834bcd0a191588e9ae79c74d64
rhennigan <rhennigan@gmail.com>
2014-12-20 02:29:23 -0500

    auto-commit changes: Sat Dec 20 02:29:23 EST 2014

--- coq/sf_basic.v
+++ coq/sf_basic.v
@@ -78,10 +78,26 @@ Definition andb3 (a b c : bool) : bool := andb a (andb b c).
 
 Example test_andb31: (andb3 true true true) = true.
 Proof.
-  
+  simpl.
+  reflexivity.
+Qed.
+
 Example test_andb32: (andb3 false true true) = false.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
 
 Example test_andb33: (andb3 true false true) = false.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
 
 Example test_andb34: (andb3 true true false) = false.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
 
+Check negb.
