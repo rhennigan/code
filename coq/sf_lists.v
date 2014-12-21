43523aa587ca21062bbaca1153707c070f49d128
rhennigan <rhennigan@gmail.com>
2014-12-21 17:27:01 -0500

    auto-commit changes: Sun Dec 21 17:27:01 EST 2014

--- coq/sf_lists.v
+++ coq/sf_lists.v
@@ -140,4 +140,15 @@ Fixpoint alternate (l1 l2 : natlist) : natlist :=
     | x :: xs, y :: ys => x :: y :: (alternate xs ys)
   end.
 
+Example test_alternate1: alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+Proof. simpl. reflexivity. Qed.
+
+Example test_alternate2: alternate [1] [4;5;6] = [1;4;5;6].
+Proof. simpl. reflexivity. Qed.
+
+Example test_alternate3: alternate [1;2;3] [4] = [1;4;2;3].
+Proof. simpl. reflexivity. Qed.
+
+Example test_alternate4: alternate [] [20;30] = [20;30].
+Proof. simpl. reflexivity. Qed.
 
