acc4a40870318b5c3e57f1e5901b3b0feab505d0
rhennigan <rhennigan@gmail.com>
2014-12-21 12:37:38 -0500

    auto-commit changes: Sun Dec 21 12:37:38 EST 2014

--- coq/sf_induction.v
+++ coq/sf_induction.v
@@ -407,6 +407,7 @@ Proof.
   intros n m p.
   rewrite -> plus_assoc.
   rewrite -> plus_assoc.
+  replace (n + m) with (m + n). rewrite -> plus_comm. reflexivity.
   assert (n + m = m + n) as H. rewrite <- plus_comm. reflexivity.
   rewrite <- H. reflexivity.
 Qed.
