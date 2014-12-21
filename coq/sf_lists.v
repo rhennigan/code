21f4b0cf4ff63cc15fc45c3e766803f48ad4a5b1
rhennigan <rhennigan@gmail.com>
2014-12-21 17:33:15 -0500

    auto-commit changes: Sun Dec 21 17:33:14 EST 2014

--- coq/sf_lists.v
+++ coq/sf_lists.v
@@ -170,4 +170,23 @@ Proof. simpl. reflexivity. Qed.
 Example test_count2: count 6 [1;2;3;1;4;1] = 0.
 Proof. simpl. reflexivity. Qed.
 
+Definition sum : bag -> bag -> bag := app.
 
+Example test_sum1: count 1 (sum [1;2;3] [1;4;1]) = 3.
+Proof. simpl. reflexivity. Qed.
+
+Definition add (v:nat) (s:bag) : bag := 
+  (* FILL IN HERE *) admit.
+
+Example test_add1: count 1 (add 1 [1;4;1]) = 3.
+ (* FILL IN HERE *) Admitted.
+Example test_add2: count 5 (add 1 [1;4;1]) = 0.
+ (* FILL IN HERE *) Admitted.
+
+Definition member (v:nat) (s:bag) : bool := 
+  (* FILL IN HERE *) admit.
+
+Example test_member1: member 1 [1;4;1] = true.
+ (* FILL IN HERE *) Admitted.
+Example test_member2: member 2 [1;4;1] = false.
+ (* FILL IN HERE *) Admitted.
