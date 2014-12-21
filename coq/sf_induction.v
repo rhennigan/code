a311485820ead79a21ddbcfbc1595f95ab3d98a5
rhennigan <rhennigan@gmail.com>
2014-12-21 12:44:48 -0500

    auto-commit changes: Sun Dec 21 12:44:48 EST 2014

--- coq/sf_induction.v
+++ coq/sf_induction.v
@@ -413,10 +413,10 @@ Proof.
   reflexivity.
 Qed.
 
-Print bin.
-
 Theorem bin_inc_comm : 
   forall b : bin, bin_to_nat (bin_inc b) = S (bin_to_nat b).
 Proof.
   intro b.
-  induction b as [|y|z].
+  induction b as [|b1|b2].
+  Case "b = S0". simpl. reflexivity.
+  Case "b = S1 b1".
