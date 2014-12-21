e1076ad9e450eec99659bf36eba6e1b4205f2c8a
rhennigan <rhennigan@gmail.com>
2014-12-21 17:03:09 -0500

    auto-commit changes: Sun Dec 21 17:03:09 EST 2014

--- coq/sf_lists.v
+++ coq/sf_lists.v
@@ -54,4 +54,11 @@ Inductive natlist : Type :=
   | cons : nat -> natlist -> natlist.
 
 Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+Notation "x :: l" := (cons x l) (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
 Print mylist.
+
+Definition mylist' := [ 1 ; 2 ; 3 ; ].
