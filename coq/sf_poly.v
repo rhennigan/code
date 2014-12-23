61f43d6f82b3d336634aec4189ef203c32934fb9
rhennigan <rhennigan@gmail.com>
2014-12-23 02:28:14 -0500

    auto-commit changes: Tue Dec 23 02:28:14 EST 2014

--- coq/sf_poly.v
+++ coq/sf_poly.v
@@ -488,7 +488,16 @@ Proof.
   reflexivity.
 Qed.
 
+Definition fold_length {X : Type} (l : list X) : nat :=
+  fold (fun _ n => S n) l 0.
 
+Example test_fold_length1 : fold_length [4;7;0] = 3.
+Proof. reflexivity. Qed.
+
+Theorem fold_length_correct :
+  forall X (l : list X),
+    fold_length l = length l.
+Proof.
 
 
 End SF_Polymorphism. 
