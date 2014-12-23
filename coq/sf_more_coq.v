7846a5201b05a7e78725b6dcff027236d32a1193
rhennigan <rhennigan@gmail.com>
2014-12-23 16:46:23 -0500

    auto-commit changes: Tue Dec 23 16:46:22 EST 2014

--- coq/sf_more_coq.v
+++ coq/sf_more_coq.v
@@ -2,14 +2,15 @@ Require Export sf_poly.
 
 Theorem beq_nat_eq : forall n m, true = beq_nat n m -> n = m.
 Proof.
-  intros n m H.
+  intros n.
   induction n as [| n'].
   Case "n = O".
   {
+    intros m H.
     destruct m as [| m']. reflexivity.
     simpl in H. inversion H.
   }
   Case "n = S n'".
   {
-    destruct m as [| m']. reflexivity.
+    destruct m as [| m']. 
   }
