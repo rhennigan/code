1bf386993907c22f7843298ff39330439c7fe2b7
rhennigan <rhennigan@gmail.com>
2014-12-23 16:42:18 -0500

    auto-commit changes: Tue Dec 23 16:42:18 EST 2014

--- coq/sf_more_coq.v
+++ coq/sf_more_coq.v
@@ -2,4 +2,9 @@ Require Export sf_poly.
 
 Theorem beq_nat_eq : forall n m, true = beq_nat n m -> n = m.
 Proof.
-  
+  intros n m H.
+  induction n as [| n'].
+  Case "n = O".
+  {
+    destruct m as [| m'].
+  }
