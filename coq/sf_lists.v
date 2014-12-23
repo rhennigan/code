7c4cc41f4fb32218404bb254b5157e73625ba09e
rhennigan <rhennigan@gmail.com>
2014-12-23 14:38:03 -0500

    auto-commit changes: Tue Dec 23 14:38:03 EST 2014

--- coq/sf_lists.v
+++ coq/sf_lists.v
@@ -573,7 +573,7 @@ Module NatList.
       assert (IHxs' : forall v : nat, negb (bpalindrome xs) = bpalindrome (v :: xs)).
       SCase "Proof of IHxs'".
       {
-        
+        apply bool_flip in (bpalindrome xs = negb (bpalindrome (v :: xs))).
       }
     }
 
