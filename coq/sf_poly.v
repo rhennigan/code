5f6e544e65951a558d158f7af1ee5f978e255ebf
rhennigan <rhennigan@gmail.com>
2014-12-22 23:08:15 -0500

    auto-commit changes: Mon Dec 22 23:08:15 EST 2014

--- coq/sf_poly.v
+++ coq/sf_poly.v
@@ -3,3 +3,8 @@ Require Export sf_lists.
 Inductive boollist : Type :=
 | bool_nil : boollist
 | bool_cons : bool -> boollist -> boollist.
+
+Inductive list' (X : Type) : Type :=
+| nil' : list' X
+| cons' : X -> list' X -> list' X.
+
