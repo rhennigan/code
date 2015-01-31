e7dfe848f4302bcc95671e349d1c3e7d3426d7ae
rhennigan <rhennigan@gmail.com>
2015-01-31 05:10:32 -0500

    auto-commit changes: Sat Jan 31 05:10:32 EST 2015

--- c/png/makePNG.c
+++ c/png/makePNG.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	// Create a test image - in this case a Mandelbrot Set fractal
 	// The output is a 1D array of doubles, length: width * height
   int n;
-  for (n = start; n < 50000; n+=8) {
+  for (n = start; n < 2000; n+=8) {
     /* rad = rad * 0.99; */
     printf("Creating Image (%d, %d, %f, %f, %f, %d)\n", width, height, dX, dY, rad * pow(0.99, (double)n), iter);
     double *buffer = createMandelbrotImage(width, height, dX, dY, rad * pow(0.99, (double)n), iter);
