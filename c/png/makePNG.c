e4ae25edca8ae64087f389f5985982667cc804b3
rhennigan <rhennigan@gmail.com>
2015-02-01 03:07:02 -0500

    auto-commit changes: Sun Feb  1 03:07:02 EST 2015

--- c/png/makePNG.c
+++ c/png/makePNG.c
@@ -230,7 +230,8 @@ double *createMandelbrotImage(int width, int height, double xS, double yS, doubl
 
 			if (iteration < maxIteration) {
 				double modZ = sqrt(x*x + y*y);
-				double mu = iteration - (log(log(modZ))) / log(2);
+				/* double mu = iteration - (log(log(modZ))) / log(2); */
+        double mu = iteration - (log(modZ)) / log(2);
 				if (mu > maxMu) maxMu = mu;
 				if (mu < minMu) minMu = mu;
 				buffer[yPos * width + xPos] = mu;
