5f8b40df975bf366a17bf0e0f61cc9feab6efd68
rhennigan <rhennigan@gmail.com>
2015-02-01 05:54:25 -0500

    auto-commit changes: Sun Feb  1 05:54:25 EST 2015

--- c/png/makePNG.c
+++ c/png/makePNG.c
@@ -118,7 +118,11 @@ const double p3 = 0.75;
 
 inline void setRGB(png_byte *ptr, double val) {
   double v = S(val, 0.0, 1.0);
-  if (v < p1) {
+  if (v <= 0.0) {
+    ptr[0] = 0;
+    ptr[1] = 0;
+    ptr[2] = 0;
+  } else if (v < p1) {
     double t2 = INT(v, 0.0, p1);
     double t1 = 1.0 - t2;
     ptr[0] = (int)(t1 * c1[0] + t2 * c2[0]);
