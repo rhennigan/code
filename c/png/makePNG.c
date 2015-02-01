122e6077202228069ed6a309c49c79a0031c6294
rhennigan <rhennigan@gmail.com>
2015-02-01 03:58:05 -0500

    auto-commit changes: Sun Feb  1 03:58:05 EST 2015

--- c/png/makePNG.c
+++ c/png/makePNG.c
@@ -28,10 +28,10 @@ inline void setRGB(png_byte *ptr, double val);
 // also written into the image file
 int writeImage(char* filename, int width, int height, double *buffer, char* title);
 
-double minVal = 0.25;
-double maxVal = 0.5;
-double minValOld = 0.25;
-double maxValOld = 0.5;
+double minVal = -0.1;
+double maxVal = 1.0;
+double minValOld = -0.1;
+double maxValOld = 1.0;
 double p = 0.1;
 
 int main(int argc, char *argv[])
