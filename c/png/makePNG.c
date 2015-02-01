251b38c92765dd0d79d9e2d81fd390c90ff92f8c
rhennigan <rhennigan@gmail.com>
2015-02-01 05:47:15 -0500

    auto-commit changes: Sun Feb  1 05:47:15 EST 2015

--- c/png/makePNG.c
+++ c/png/makePNG.c
@@ -112,9 +112,9 @@ const int c2[3] = { 12,  90, 129};
 const int c3[3] = {215, 224, 229};
 const int c4[3] = {214,  74,  39};
 const int c5[3] = {142, 173, 171};
-const double p1 = 0.15;
-const double p2 = 0.25;
-const double p3 = 0.26;
+const double p1 = 0.25;
+const double p2 = 0.50;
+const double p3 = 0.75;
 
 inline void setRGB(png_byte *ptr, double val) {
   double v = S(val, 0.0, 1.0);
