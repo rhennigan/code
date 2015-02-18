bd8441af0151790f0f948eae8ca2af672ed0a6bc
rhennigan <rhennigan@gmail.com>
2015-02-18 16:19:02 -0500

    auto-commit changes: Wed Feb 18 16:19:02 EST 2015

--- classes/cs546/list.js
+++ classes/cs546/list.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var curry = function (fn) {
-	var args = Array.prototype.slice.call(arguments, 1);
-	return function () {
-		return fn.apply(this, args.concat(
-			Array.prototype.slice.call(arguments, 0)));
-	};
+  var args = Array.prototype.slice.call(arguments, 1);
+  return function () {
+    return fn.apply(this, args.concat(
+      Array.prototype.slice.call(arguments, 0)));
+  };
 };
 
 function List (x, xs) {
