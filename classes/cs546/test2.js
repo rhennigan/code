831c5a627f77a1f84fcfeed5014a42a4eaa8ff72
rhennigan <rhennigan@gmail.com>
2015-02-18 15:06:52 -0500

    auto-commit changes: Wed Feb 18 15:06:52 EST 2015

--- classes/cs546/test2.js
+++ classes/cs546/test2.js
@@ -311,7 +311,7 @@ List.map = function (f) {
 };
 
 List.N = function (n) {
-	return n <= 0 ? new List() : List.join (List.N (n-1)) (new List (n, null));
+	return n <= 0 ? new List() : List.cons (n) (List.N (n-1));
 };
 
 // List.map = function (f) {
