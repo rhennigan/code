81e3623e6e541590fa8f14b120302eba0ac323c3
rhennigan <rhennigan@gmail.com>
2015-02-17 22:44:22 -0500

    auto-commit changes: Tue Feb 17 22:44:22 EST 2015

--- classes/cs546/test2.js
+++ classes/cs546/test2.js
@@ -230,6 +230,11 @@ function splitLine (line) {
 		var sq3 = Math.sqrt(3.0);
 		m.x = 0.5 * (m1.x + sq3 * m1.y + m2.x - sq3 * m2.y);
 		m.y = 0.5 * (-sq3 * m1.x + m1.y + sq3 * m2.x + m2.y);
+
+		var newLines = [new Line(line.pt1, m1, line.col1, line.col1),
+										new Line(line.pt1, m1, line.col1, line.col1),
+										new Line(line.pt1, m1, line.col1, line.col1),
+										new Line(line.pt1, m1, line.col1, line.col1)];
 }
 
 function updateCanvas (data) {
