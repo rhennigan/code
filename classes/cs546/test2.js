f65205569342db809d6210ee3a7fc2cc91817a48
rhennigan <rhennigan@gmail.com>
2015-02-17 21:35:59 -0500

    auto-commit changes: Tue Feb 17 21:35:59 EST 2015

--- classes/cs546/test2.js
+++ classes/cs546/test2.js
@@ -116,6 +116,85 @@ function drawPixelAA (point, c, colorB) {
 		canvasData.data[index + 3] = pixelc.a;
 }
 
+function ipart(x) {
+    return Math.floor(x);
+}
+
+function round(x) {
+    return Math.round(x);
+}
+
+function fpart(x) {
+    return (x < 0) ? (1 - (x - Math.floor(x))) : (x - Math.floor(x));
+}
+
+function rfpart(x) {
+    return 1 - fpart(x);
+}
+
+function drawLineAA (line) {
+		var x0 = line.pt1.x;
+		var y0 = line.pt1.y;
+		var x1 = line.pt2.x;
+		var y1 = line.pt2.y;
+		
+    var steep = Boolean(Math.abs(y1 - y0) > Math.abs(x1 - x0));
+    
+    if (steep) {
+        y0 = [x0, x0 = y0][0];
+        y1 = [x1, x1 = y1][0];
+    }
+    if (x0 > x1) {
+        x1 = [x0, x0 = x1][0];
+        y1 = [y0, y0 = y1][0];
+    }
+
+    var dx = x1 - x0;
+    var dy = y1 - y0;
+    var gradient = dy / dx;
+
+    // handle first endpoint
+    var xend = Math.round(x0);
+    var yend = y0 + gradient * (xend - x0);
+    var xgap = rfpart(x0 + 0.5);
+    var xpxl1 = xend; // this will be used in the main loop
+    var ypxl1 = ipart(yend);
+    if (steep) {
+        plot(ypxl1    , xpxl1, rfpart(yend) * xgap, r, g, b, a);
+        plot(ypxl1 + 1, xpxl1,  fpart(yend) * xgap, r, g, b, a);
+    } else {
+        plot(xpxl1, ypxl1    , rfpart(yend) * xgap, r, g, b, a);
+        plot(xpxl1, ypxl1 + 1,  fpart(yend) * xgap, r, g, b, a);
+    }
+    var intery = yend + gradient;
+
+    // handle second endpoint
+    xend = Math.round(x1);
+    yend = y1 + gradient * (xend - x1);
+    xgap = fpart(x1 + 0.5);
+    var xpxl2 = xend; //this will be used in the main loop
+    var ypxl2 = ipart(yend);
+    if (steep) {
+        plot(ypxl2  , xpxl2, rfpart(yend) * xgap, r, g, b, a);
+        plot(ypxl2+1, xpxl2,  fpart(yend) * xgap, r, g, b, a);
+    } else {
+        plot(xpxl2, ypxl2,  rfpart(yend) * xgap, r, g, b, a);
+        plot(xpxl2, ypxl2+1, fpart(yend) * xgap, r, g, b, a);
+    }
+
+    // main loop
+    for (x = xpxl1+1; x <= xpxl2; x++) {
+        if (steep) {
+            plot(ipart(intery)  , x, rfpart(intery), r, g, b, a);
+            plot(ipart(intery)+1, x,  fpart(intery), r, g, b, a);
+        } else {
+            plot(x, ipart(intery)  , rfpart(intery), r, g, b, a);
+            plot(x, ipart(intery)+1,  fpart(intery), r, g, b, a);
+        }
+        intery = intery + gradient;
+    }
+}
+
 function updateCanvas (data) {
     ctx.putImageData(data, 0, 0);
 }
