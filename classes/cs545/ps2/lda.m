8c83ee458ef70a3c1192d1dc1d250c61e029b26c
rhennigan <rhennigan@gmail.com>
2015-02-23 13:16:19 -0500

    auto-commit changes: Mon Feb 23 13:16:19 EST 2015

--- classes/cs545/ps2/lda.m
+++ classes/cs545/ps2/lda.m
@@ -39,7 +39,7 @@ for i = 1:length(X)
   end
 end
 
-accuracy = 1 - sum(abs(t-y)) / length(y);
+accuracy = 100 * (1 - sum(abs(t-y)) / length(y));
 fprintf('Training Accuracy of LDA is: %d \n', accuracy);
 
 % Get w and training accuracy
