b6d79da407e555591fa328d79bf8de9f0ab4fa26
rhennigan <rhennigan@gmail.com>
2015-02-23 13:30:30 -0500

    auto-commit changes: Mon Feb 23 13:30:30 EST 2015

--- classes/cs545/ps2/lda.m
+++ classes/cs545/ps2/lda.m
@@ -1,35 +1,38 @@
 function [w, accuracy] = lda(X, y)
 % Linear Discriminant Analysis
 
-% get the pos and neg data
+
 % pos_data = X(find(y), :)
 % neg_data = X(find(1 - y), :)
 
-% % Mean of each class
+% 
 % pos_mean = mean(pos_data)
 % neg_mean = mean(neg_data)
 
-% % Center the data
+% 
 % pos_data = bsxfun(@minus, pos_data, mean([pos_mean; neg_mean]))
 % neg_data = bsxfun(@minus, neg_data, mean([pos_mean; neg_mean]))
 
 % % Covariance of the data
 % cov_all = (pos_data' * pos_data + neg_data' * neg_data) / length(X)
 
+% get the pos and neg data
 X1 = X(find(y), :);
 X2 = X(find(1 - y), :);
 
+% Mean of each class
 m1 = mean(X1);
 m2 = mean(X2);
 
-M1 = bsxfun(@minus, X1, m1);
-M2 = bsxfun(@minus, X2, m2);
+% Center the data
+C1 = bsxfun(@minus, X1, m1);
+C2 = bsxfun(@minus, X2, m2);
 
 n1 = length(X1);
 n2 = length(X2);
 
-S1 = M1' * M1 / n1;
-S2 = M2' * M2 / n2;
+S1 = C1' * C1 / n1;
+S2 = C2' * C2 / n2;
 Sw = (S1 + S2) / 2;
 w0 = inv(Sw) * (m1 - m2)';
 w  = w0 / norm(w0);
