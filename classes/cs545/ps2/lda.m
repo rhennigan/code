0a4e5f1a2745f2aebf39885ff164722334dc24a8
rhennigan <rhennigan@gmail.com>
2015-02-23 13:32:33 -0500

    auto-commit changes: Mon Feb 23 13:32:33 EST 2015

--- classes/cs545/ps2/lda.m
+++ classes/cs545/ps2/lda.m
@@ -1,54 +1,40 @@
-function [w, accuracy] = lda(X, y)
 % Linear Discriminant Analysis
 
+function [w, accuracy] = lda(X, y)
 
-% pos_data = X(find(y), :)
-% neg_data = X(find(1 - y), :)
+  % get the pos and neg data
+  X1 = X(find(y), :);
+  X2 = X(find(1 - y), :);
 
-% 
-% pos_mean = mean(pos_data)
-% neg_mean = mean(neg_data)
+  % Mean of each class
+  m1 = mean(X1);
+  m2 = mean(X2);
 
-% 
-% pos_data = bsxfun(@minus, pos_data, mean([pos_mean; neg_mean]))
-% neg_data = bsxfun(@minus, neg_data, mean([pos_mean; neg_mean]))
+  % Center the data
+  C1 = bsxfun(@minus, X1, m1);
+  C2 = bsxfun(@minus, X2, m2);
 
-% 
-% cov_all = (pos_data' * pos_data + neg_data' * neg_data) / length(X)
+  % Covariance of the data
+  n1 = length(X1);
+  n2 = length(X2);
+  S1 = C1' * C1 / n1;
+  S2 = C2' * C2 / n2;
+  Sw = (S1 + S2) / 2;
 
-% get the pos and neg data
-X1 = X(find(y), :);
-X2 = X(find(1 - y), :);
+  % Get w and training accuracy
+  w0 = inv(Sw) * (m1 - m2)';
+  w  = w0 / norm(w0);
 
-% Mean of each class
-m1 = mean(X1);
-m2 = mean(X2);
+  % t = (X-mean(X)) * w;
+  t = bsxfun(@minus, X, 0.5 * (m1 + m2)) * w;
+  t(find(t<0)) = 0;
+  t(find(t>0)) = 1;
+  accuracy = 100 * (1 - sum(abs(t-y)) / length(y));
 
-% Center the data
-C1 = bsxfun(@minus, X1, m1);
-C2 = bsxfun(@minus, X2, m2);
+  % Plot Gaussian Ellipsoids
+  h_pos = plot_gaussian_ellipsoid(m1, Sw);
+  h_neg = plot_gaussian_ellipsoid(m2, Sw);
+  set(h_pos,'color','r');
+  set(h_neg,'color','g');
 
-n1 = length(X1);
-n2 = length(X2);
-
-% Covariance of the data
-S1 = C1' * C1 / n1;
-S2 = C2' * C2 / n2;
-Sw = (S1 + S2) / 2;
-
-% Get w and training accuracy
-w0 = inv(Sw) * (m1 - m2)';
-w  = w0 / norm(w0);
-
-% t = (X-mean(X)) * w;
-t = bsxfun(@minus, X, 0.5 * (m1 + m2)) * w;
-t(find(t<0)) = 0;
-t(find(t>0)) = 1;
-accuracy = 100 * (1 - sum(abs(t-y)) / length(y));
-
-% Plot Gaussian Ellipsoids
-h_pos = plot_gaussian_ellipsoid(m1, Sw);
-h_neg = plot_gaussian_ellipsoid(m2, Sw);
-set(h_pos,'color','r');
-set(h_neg,'color','g');
 end
