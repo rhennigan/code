324bfc75bad9470686e419793c3546568efacc6e
rhennigan <rhennigan@gmail.com>
2015-02-23 10:12:11 -0500

    auto-commit changes: Mon Feb 23 10:12:11 EST 2015

--- classes/cs545/ps2/costFunctionReg.m
+++ classes/cs545/ps2/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = -1/m * sum(y .* log(sigmoid(X*theta))+(1.- y).*log(1.- sigmoid(X*theta))) + ...
+  lambda/(2*m) * theta(2:end)'*theta(2:end);
 
 
+grad(1) = 1/m * (sigmoid(X*theta)-y)'*X(:,1);
 
-
+grad(2:end) = 1/m * ((sigmoid(X*theta)-y)'*X(:,2:end))' + lambda/m * theta(2:end);
 
 % =============================================================
 
