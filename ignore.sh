bfb34a974b62d6f9a37f792a7b434eebe871ef5d
rhennigan <rhennigan@gmail.com>
2015-02-23 09:45:27 -0500

    update

--- ignore.sh
+++ ignore.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+FILES=$(git status -s | sed 's/ M //g' | sed 's/A  //g' | sed 's/?? //g' | sed 's/\"//g' | grep $1)
 
-git status -s | grep '?? ' | sed 's/?? //g' | sed 's/\#/\\#/g' >> .gitignore
+for file in $FILES
+do
+	echo $file >> .gitignore
+done
+
+IFS=$SAVEIFS
