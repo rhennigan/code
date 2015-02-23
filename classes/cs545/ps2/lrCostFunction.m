ad11f72bda965c91e3e9d5b5da3ddf84324ab669
rhennigan <rhennigan@gmail.com>
2015-02-23 13:58:12 -0500

    auto-commit changes: Mon Feb 23 13:58:12 EST 2015

--- classes/cs545/ps2/lrCostFunction.m
+++ classes/cs545/ps2/lrCostFunction.m
@@ -36,12 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-J = -1/m * sum(y .* log(sigmoid(X*theta))+(1.- y).*log(1.- sigmoid(X*theta))) + ...
-  lambda/(2*m) * theta(2:end)'*theta(2:end);
+J = - 1 / m * sum(y .* log(sigmoid(X * theta)) + (1 .- y) .* log(1 .- sigmoid(X * theta))) ...
+    + lambda / (2 * m) * theta(2:end)' * theta(2:end);
 
-grad(1) = 1/m * (sigmoid(X*theta)-y)'*X(:,1);
+grad(1) = 1 / m * (sigmoid(X * theta) - y)' * X(:, 1);
 
-grad(2:end) = 1/m * ((sigmoid(X*theta)-y)'*X(:,2:end))' + lambda/m * theta(2:end);
+grad(2:end) = 1 / m * ((sigmoid(X * theta) - y)' * X(:, 2:end))' + lambda / m * theta(2:end);
 
 % =============================================================
 
