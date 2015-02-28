469061baaaeee7b7cadb71f8a765781a0cdfce80
rhennigan <rhennigan@gmail.com>
2015-02-28 16:43:50 -0500

    auto-commit changes: Sat Feb 28 16:43:51 EST 2015

--- html/fnoise/fragment.glsl
+++ html/fnoise/fragment.glsl
@@ -64,9 +64,9 @@ vec4 shuffle(vec4 v) {
 ////////////////////////////////////////////////////////////////////////////////
 
 vec4 color_px(float val, float p) {
-  const float p1 = 0.25;
+  const float p1 = 0.40;
   const float p2 = 0.50;
-  const float p3 = 0.75;
+  const float p3 = 0.60;
 
   vec4 col = vec4(0.0, 0.0, 0.0, 1.0);
   float v = S(val, 0.0, 1.0);
@@ -283,7 +283,7 @@ float perlin_noise(vec3 P) {
 ////////////////////////////////////////////////////////////////////////////////
 
 void main(void) {
-  float noise = perlin_noise(vec3(pos.x + p, pos.y + p, p));
+  float noise = perlin_noise(vec3(aspect*pos.x + p, pos.y + p, p));
   vec4 color = color_px(noise, p);
   gl_FragColor = color;
 }
