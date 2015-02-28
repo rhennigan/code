c8d6f81b5ab4ce2ae7973220f35d66f2b648a313
rhennigan <rhennigan@gmail.com>
2015-02-28 18:36:11 -0500

    auto-commit changes: Sat Feb 28 18:36:12 EST 2015

--- html/fnoise/fragment.glsl
+++ html/fnoise/fragment.glsl
@@ -288,10 +288,10 @@ float billow(vec3 P, float f, float lac, float per, float seed) {
 
 void main(void) {
   vec3 s_pos = vec3(aspect*pos.x + p, pos.y + p, p);
-  vec3 ps = turbulence_shift(s_pos, 0.02, 1);
+  vec3 ps = turbulence_shift(s_pos, 0.05, 1);
   float noise = perlin_noise(ps);
   float b_noise = billow(s_pos, 0.2, 2.0, 0.5, 1.0);
-  vec4 color = color_px(0.5*(noise+b_noise), p);
+  vec4 color = color_px((noise+b_noise+noise*b_noise)/3.0, p);
   gl_FragColor = color;
 }
 
