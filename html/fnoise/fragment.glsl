455759d98ea9c68e0ba8d9a542f7299b18f5d78c
rhennigan <rhennigan@gmail.com>
2015-02-28 17:32:44 -0500

    auto-commit changes: Sat Feb 28 17:32:45 EST 2015

--- html/fnoise/fragment.glsl
+++ html/fnoise/fragment.glsl
@@ -150,32 +150,32 @@ float snoise(vec3 v) {
 
   vec4 j = p - NN * floor(p * ns.z *ns.z);  //  mod(p,N*N)
 
-  vec4 x_ = floor( j * ns.z );
-  vec4 y_ = floor( j - N * x_ );    // mod(j,N)
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - N * x_);    // mod(j,N)
 
   vec4 x = x_ *ns.x + ns.yyyy;
   vec4 y = y_ *ns.x + ns.yyyy;
-  vec4 h = 1.0 - abs( x ) - abs( y );
+  vec4 h = 1.0 - abs(x) - abs(y);
 
-  vec4 b0 = vec4( x.xy, y.xy );
-  vec4 b1 = vec4( x.zw, y.zw );
+  vec4 b0 = vec4(x.xy, y.xy);
+  vec4 b1 = vec4(x.zw, y.zw);
 
 
-  vec4 s0 = floor( b0 ) * 2.0 + 1.0;
-  vec4 s1 = floor( b1 ) * 2.0 + 1.0;
-  vec4 sh = -step( h, vec4( 0.0 ) );
+  vec4 s0 = floor(b0) * 2.0 + 1.0;
+  vec4 s1 = floor(b1) * 2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
 
   vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
   vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
 
-  vec3 p0 = vec3( a0.xy, h.x );
-  vec3 p1 = vec3( a0.zw, h.y );
-  vec3 p2 = vec3( a1.xy, h.z );
-  vec3 p3 = vec3( a1.zw, h.w );
+  vec3 p0 = vec3(a0.xy, h.x);
+  vec3 p1 = vec3(a0.zw, h.y);
+  vec3 p2 = vec3(a1.xy, h.z);
+  vec3 p3 = vec3(a1.zw, h.w);
 
   // Normalise gradients
 
-  vec4 norm = taylorInvSqrt( vec4( dot( p0, p0 ), dot( p1, p1 ), dot( p2, p2 ), dot( p3, p3 ) ) );
+  vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
@@ -183,10 +183,10 @@ float snoise(vec3 v) {
 
   // Mix final noise value
 
-  vec4 m = max( 0.6 - vec4( dot( x0, x0 ), dot( x1, x1 ), dot( x2, x2 ), dot( x3, x3 ) ), 0.0 );
+  vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
   m = m * m;
-  return 42.0 * dot( m*m, vec4( dot( p0, x0 ), dot( p1, x1 ),
-    dot( p2, x2 ), dot( p3, x3 ) ) );
+  return 42.0 * dot(m*m, vec4(dot(p0, x0), dot(p1, x1),
+    dot(p2, x2), dot(p3, x3)));
 
 }
 
