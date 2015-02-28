21d13166767f9a2ed8b5b8812c391033d0e57767
rhennigan <rhennigan@gmail.com>
2015-02-28 17:34:47 -0500

    auto-commit changes: Sat Feb 28 17:34:48 EST 2015

--- html/fnoise/fragment.glsl
+++ html/fnoise/fragment.glsl
@@ -119,21 +119,17 @@ float snoise(vec3 v) {
   const vec2 C = vec2(1.0/6.0, 1.0/3.0);
   const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
 
-  // First corner
   vec3 i  = floor(v + dot(v, C.yyy));
   vec3 x0 = v - i + dot(i, C.xxx);
 
-  // Other corners
   vec3 g = step(x0.yzx, x0.xyz);
   vec3 l = 1.0 - g;
   vec3 i1 = min(g.xyz, l.zxy);
   vec3 i2 = max(g.xyz, l.zxy);
 
-  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
-  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
-  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
-
-  // Permutations
+  vec3 x1 = x0 - i1  + 1.0 * C.xxx;
+  vec3 x2 = x0 - i2  + 2.0 * C.xxx;
+  vec3 x3 = x0 - 1.0 + 3.0 * C.xxx;
 
   i = mod(i, WRAP);
   vec4 p = shuffle(shuffle(shuffle(
@@ -141,17 +137,14 @@ float snoise(vec3 v) {
   + i.y + vec4(0.0, i1.y, i2.y, 1.0))
   + i.x + vec4(0.0, i1.x, i2.x, 1.0));
 
-  // Gradients
-  // ( N*N points uniformly over a square, mapped onto an octahedron.)
-
-  float n_ = 1.0 / N; // N=7
+  float n_ = 1.0 / N;
 
   vec3 ns = n_ * D.wyz - D.xzx;
 
-  vec4 j = p - NN * floor(p * ns.z *ns.z);  //  mod(p,N*N)
+  vec4 j = p - NN * floor(p * ns.z *ns.z);
 
   vec4 x_ = floor(j * ns.z);
-  vec4 y_ = floor(j - N * x_);    // mod(j,N)
+  vec4 y_ = floor(j - N * x_);
 
   vec4 x = x_ *ns.x + ns.yyyy;
   vec4 y = y_ *ns.x + ns.yyyy;
@@ -173,20 +166,15 @@ float snoise(vec3 v) {
   vec3 p2 = vec3(a1.xy, h.z);
   vec3 p3 = vec3(a1.zw, h.w);
 
-  // Normalise gradients
-
   vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
   p0 *= norm.x;
   p1 *= norm.y;
   p2 *= norm.z;
   p3 *= norm.w;
 
-  // Mix final noise value
-
   vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
   m = m * m;
-  return 42.0 * dot(m*m, vec4(dot(p0, x0), dot(p1, x1),
-    dot(p2, x2), dot(p3, x3)));
+  return 42.0 * dot(m*m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
 
 }
 
