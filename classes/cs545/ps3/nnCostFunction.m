69b0bb1db329b5af0b35029cc62c271983a6331f
rhennigan <rhennigan@gmail.com>
2015-03-24 13:58:31 -0400

    auto-commit changes: Tue Mar 24 13:58:31 EDT 2015

--- classes/cs545/ps3/nnCostFunction.m
+++ classes/cs545/ps3/nnCostFunction.m
@@ -62,42 +62,38 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-costmatrix = zeros(m,num_labels);
-
-%for i=1:m,
-%    out(i,y(i))=1;
-%end
-out=y;
-h = sigmoid([ones(m,1) sigmoid([ones(m,1) X] * Theta1')] * Theta2');
-
-for i = 1:m,
-    for k = 1:size(y,2),
-        costmatrix(i,k) = -out(i,k) * log(h(i,k))-(1-out(i,k))*log(1-h(i,k));
-    end
-end
-
-J = sum(sum(costmatrix)) / m + ...
-    (lambda / (2*m)) * (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
-    
-
-for t=1:m,
-    a1 = [1 X(t,:)]';
-    
-    z2 = Theta1 * a1;
-    a2 = [1; sigmoid(z2)];
-    
-    z3 = Theta2 * a2;
-    a3 = sigmoid(z3);
-    
-    delta3 = a3 - out(t,:)';
-    delta2 = Theta2(:,2:end)' * delta3.*sigmoidGradient(z2);
-        
-    Theta1_grad = Theta1_grad + delta2*a1';
-    Theta2_grad = Theta2_grad + delta3*a2';
-end
-
-Theta1_grad = [Theta1_grad(:,1)/m Theta1_grad(:,2:end)/m+(lambda/m)*Theta1(:,2:end)];
-Theta2_grad = [Theta2_grad(:,1)/m Theta2_grad(:,2:end)/m+(lambda/m)*Theta2(:,2:end)];
+X = [ones(m, 1) X];
+y = eye(num_labels)(y,:);
+ 
+ 
+a1 = X;
+ 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+ 
+n = size(a2, 1);
+a2 = [ones(n,1) a2];
+ 
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+ 
+regularization = (lambda/(2*m)) * (sum(sum((Theta1(:,2:end)).^2)) + sum(sum((Theta2(:,2:end)).^2)));
+ 
+J = ((1/m) * sum(sum((-y .* log(a3))-((1-y) .* log(1-a3))))) + regularization;
+ 
+ 
+delta_3 = a3 - y;
+delta_2 = (delta_3 * Theta2(:,2:end)) .* sigmoidGradient(z2);
+ 
+ 
+delta_cap2 = delta_3' * a2; 
+delta_cap1 = delta_2' * a1;
+ 
+Theta1_grad = ((1/m) * delta_cap1) + ((lambda/m) * (Theta1));
+Theta2_grad = ((1/m) * delta_cap2) + ((lambda/m) * (Theta2));
+ 
+Theta1_grad(:,1) -= ((lambda/m) * (Theta1(:,1)));
+Theta2_grad(:,1) -= ((lambda/m) * (Theta2(:,1)));
 
 % -------------------------------------------------------------
 
