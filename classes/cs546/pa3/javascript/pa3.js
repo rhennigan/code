5f4a1ee30294d9bcd82820fe912586f888634c9f
rhennigan <rhennigan@gmail.com>
2015-04-13 22:35:09 -0400

    auto-commit changes: Mon Apr 13 22:35:09 EDT 2015

--- classes/cs546/pa3/javascript/pa3.js
+++ classes/cs546/pa3/javascript/pa3.js
@@ -443,7 +443,7 @@
       results = [];
       for (k = 0, len1 = ref.length; k < len1; k++) {
         v = ref[k];
-        console.log(v.x);
+        console.log(m[0][3]);
         results.push({
           x: m[1][4] + m[1][1] * v.x + m[1][2] * v.y + m[1][3] * v.z,
           y: m[2][4] + m[2][1] * v.x + m[2][2] * v.y + m[2][3] * v.z
